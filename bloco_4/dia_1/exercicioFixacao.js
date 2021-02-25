2375f7f8378f54cfea762cd6da74ffaa39388025
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-02-24 23:15:44 -0300

    Alteraçao na variavel birthYear

--- bloco_4/dia_1/exercicioFixacao.js
+++ bloco_4/dia_1/exercicioFixacao.js
@@ -1,6 +1,11 @@
-const name = "Carlos Henrique Scalambrine de Souza"
-console.log(name)
+const name = "Carlos Henrique Scalambrine de Souza";
 
-const birthCity = "São Paulo"
-console.log(birthCity)
+const birthCity = "São Paulo";
 
+let birthYear = 1987;
+
+birthYear = 2020;
+
+console.log(birthYear)
+
+console.log(" Meu nome é : " + name, "nascido no ano de" , birthYear,  "residindo em " + birthCity);
