e2ce8daaab793dcaea9dc9bed21cba5ceef203d6
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-01 15:50:06 -0300

    Add arquivo exerTipOp com as respectivos operaçoes

---
+++ bloco_4/dia_1/exerTipOp.js
@@ -0,0 +1,10 @@
+let base = 5;
+let altura = 8;
+let = area = base * altura;
+console.log(area);
+
+let ladoA = 5;
+let ladoB = 5;
+let ladoC = 5;
+let = perimetro = (ladoA + ladoB + (ladoC));
+console.log(perimetro); 
