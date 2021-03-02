dcfdbd74f451305ca31c7c73874c543b093da131
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-01 23:28:55 -0300

    Add ultimo exercicio exemplificando o uso do switchcase

---
+++ bloco_4/dia_1/switchCase.js
@@ -0,0 +1,18 @@
+let situation = "reprovada";
+
+switch(situation){
+    case "aprovada":
+    console.log("Parabens")
+    break;
+
+    case "lista":
+    console.log("Estude um pouco mais")
+    break;
+
+    case "reprovada":
+    console.log("Infelizmente não foi desta vez")
+    break;
+    
+    default:
+    console.log("Nenhuma das alternativas");
+}
