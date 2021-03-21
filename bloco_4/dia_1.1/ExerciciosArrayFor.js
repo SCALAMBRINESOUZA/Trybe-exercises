ac3e93b6dc4a15e4d93fdbbb823021f904281f38
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-21 02:40:45 -0300

    Exercicio somando os valores do array

--- bloco_4/dia_1.1/ExerciciosArrayFor.js
+++ bloco_4/dia_1.1/ExerciciosArrayFor.js
@@ -1,4 +1,12 @@
+// Exercicio 1 //
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for(let number of numbers){
+//     console.log(number);
+// }
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for(let number of numbers){
-    console.log(number);
-}
+let sum = 0;
+for(let number = 0; number < numbers.length; number += 1){
+    sum += numbers[number];
+ }
+ console.log(sum);
