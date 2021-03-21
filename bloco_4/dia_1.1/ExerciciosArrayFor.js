4fe08b16c5068de1362b273038a5875f5ce6d99a
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-21 02:56:03 -0300

    Imprimindo a media aritmetica dos valores do array

--- bloco_4/dia_1.1/ExerciciosArrayFor.js
+++ bloco_4/dia_1.1/ExerciciosArrayFor.js
@@ -4,9 +4,19 @@
 //     console.log(number);
 // }
 
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum = 0;
+// for(let number = 0; number < numbers.length; number += 1){
+//     sum += numbers[number];
+//  }
+//  console.log(sum);
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
+let average = 0;
+
 for(let number = 0; number < numbers.length; number += 1){
     sum += numbers[number];
- }
- console.log(sum);
+    average = sum / 10;
+}
+    console.log(average);
