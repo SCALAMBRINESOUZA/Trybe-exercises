3141411127b95d187797b0bddd4649387d8f98c9
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-26 02:16:32 -0300

    Segundo exemplo

---
+++ bloco_4/dia_3/exercicios_fixacao/secondExample.js
@@ -0,0 +1,19 @@
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge' 
+//   } 
+
+//   for(let name in names){   
+//     console.log(`Olá ${names[name]}`)
+//   }
+
+let carro = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+}
+
+for(let carros in carro){
+    console.log(`${carros} -->> ${carro[carros]}`);
+}
