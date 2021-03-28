6d22860d46bdec45d160f8e75844db98bb3b8e57
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-28 03:07:55 -0300

    Primeiro exemplo

---
+++ bloco_5/script.js
@@ -0,0 +1,11 @@
+var paragraph = document.getElementById("paragraph");
+    paragraph.style.color = "red";
+
+let title = document.querySelector('#page-title');
+    title.innerText = 'Bullet';
+    
+let secondParagraph = document.querySelector('#second-paragraph');
+    secondParagraph.innerHTML = "HELLO WORLD DOM"
+
+let subTitle = document.querySelector('#subtitle');
+    subTitle.innerText = 'Let´s go learn javascript'    
