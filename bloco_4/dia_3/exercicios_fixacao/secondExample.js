2724aaf0c74ff8d152ed1a098ed29397e3ea6b12
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-28 05:01:16 -0300

    update

--- bloco_4/dia_3/exercicios_fixacao/secondExample.js
+++ bloco_4/dia_3/exercicios_fixacao/secondExample.js
@@ -8,14 +8,26 @@
 //     console.log(`Olá ${names[name]}`)
 //   }
 
-const carro = {
-    model: 'A3 Sedan',
-    manufacturer: 'Audi',
-    year: 2020
-}
+// const carro = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+// }
 
-for(let carros in carro){
-    console.log(`${carros} -->> ${carro[carros]}`);
-}
-// alter no git hub //
+// for(let carros in carro){
+//     console.log(`${carros} -->> ${carro[carros]}`);
+// }
+                        // Objeto com metodos //
+
+const sum = {
+    number1: 15,
+    number2: 25,
+    soma: function() { 
+     return this.number1 + this.number2
+    }
+};
+    console.log(sum.soma());
+
+
+
 
