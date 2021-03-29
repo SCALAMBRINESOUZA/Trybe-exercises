5db8b33827688fb6f5489814cf3247328e135c56
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-28 22:57:34 -0300

    alterado container de vermelho para branco e corrigido o texto no h1

--- bloco_5/dia_5.1/scriptDom.js
+++ bloco_5/dia_5.1/scriptDom.js
@@ -3,3 +3,8 @@ firstParagraph.innerText = 'Um bom programador com emprego satisfatorio';
 
 mainContainer = document.getElementsByClassName('main-content')[0]
 mainContainer.style.background = "rgb(76,164,109)";
+
+whiteContainer = document.querySelector('.center-content');
+whiteContainer.style.background = 'white';
+
+hedH1 = document.getElementsByTagName('h1')[0].innerText = 'Exercício 5.1 - Javascripit'
