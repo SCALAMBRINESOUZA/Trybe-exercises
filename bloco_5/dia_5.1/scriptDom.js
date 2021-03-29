b3855856c2b8ed8a391404ef4aefb26eb6051879
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-28 22:52:10 -0300

    Alterado paragrafo para meu texto e a cor do container der amarelo para verde

---
+++ bloco_5/dia_5.1/scriptDom.js
@@ -0,0 +1,5 @@
+firstParagraph = document.getElementById('desc');
+firstParagraph.innerText = 'Um bom programador com emprego satisfatorio';
+
+mainContainer = document.getElementsByClassName('main-content')[0]
+mainContainer.style.background = "rgb(76,164,109)";
