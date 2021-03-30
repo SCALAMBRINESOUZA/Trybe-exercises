13860f91fbedcf68d04cc171a82212b385ab46f0
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-29 22:11:51 -0300

    Add div como filho de body

--- bloco_5/exercicios_fixacao/script_5.2.js
+++ bloco_5/exercicios_fixacao/script_5.2.js
@@ -1,4 +1,13 @@
-let h1 = document.createElement('h1')
-h1.innerText = ' Exercício 5.2 - JavaScript DOM';
-h1.setAttribute('class', 'headers');
-document.body.appendChild(h1);    
+const title = document.createElement('h1');
+title.innerText = ' Exercício 5.2 - JavaScript DOM';
+title.setAttribute('class', 'headers');
+document.body.appendChild(title);    
+
+const dadContainer = document.createElement('div'); 
+dadContainer.setAttribute('class', 'main-content');
+document.body.appendChild(dadContainer);
+
+const childContainer = document.createElement('div');
+childContainer.setAttribute('class', 'center-content');
+dadContainer.appendChild(childContainer);
+
