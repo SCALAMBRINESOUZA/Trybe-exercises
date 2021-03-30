a13113ec91e9abdd89516968061c21afc5400d37
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-30 09:58:18 -0300

    exercicio encontrando maior palavra no array

--- bloco_7/dia_1/fatorial.js
+++ bloco_7/dia_1/fatorial.js
@@ -1,8 +1,3 @@
 const factoring = number => number > 1 ? number * factoring(number - 1) : 1;
-<<<<<<< HEAD
-console.log(factoring(5)); 
-///
-=======
-console.log(factoring(5));
+console.log(factoring(6)); 
 
->>>>>>> 68993a4184a5bff225ba456e16b5d92058c49f0e
