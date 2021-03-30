77f82a3c70d797326a27b7cce084e398c9672eff
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-30 08:31:49 -0300

    exemplos com o uso de arrow functions

--- bloco_7/dia_1/exercicios_fixacao.js
+++ bloco_7/dia_1/exercicios_fixacao.js
@@ -68,12 +68,40 @@
 // // Sem o template literals:
 // console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n')
 
+/*
+                        Definições do uso de template literals
+-----------------------------------------------------------------------------------------
+                                                                                          */
+                           
+//    sem arrow function 
 
+// const printName = function () {
+//     const myName = "Lucas";
+//     return myName;
+//   }
+//   console.log(printName());
 
+// com arrow function
 
+//  const printName = () => {
+//    const myName = "Lucas";
+//    return myName;
+//  }
+//   console.log(printName());
 
+//   const printName = () => "Lucas";
+//   console.log(printName());
 
+//   const multiplyByTwo = number => number * 2;
+//   console.log(multiplyByTwo(10));  
 
+//   const multiplyByTwo = number => number * 2;
+//   console.log(multiplyByTwo(10));  
 
+//   const multiplication = (number, multi) => number * multi;
+//   console.log(multiplication(8, 2));
 
-
+/*
+                        Definições do uso de arrow functions
+-----------------------------------------------------------------------------------------
+                                                                                          */
