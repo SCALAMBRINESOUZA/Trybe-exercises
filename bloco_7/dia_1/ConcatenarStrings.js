cc681aa4a632fbfbcdb2d2a43c4b24a4197758eb
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-30 21:43:09 -0300

    Seguindo baby steps localizado a letra x com metodo includes

--- bloco_7/dia_1/ConcatenarStrings.js
+++ bloco_7/dia_1/ConcatenarStrings.js
@@ -1,3 +1,17 @@
 const changeString = (string) => {
-  
+  let phrase = []
+  let ph = ''
+  for(let index = 0; index < string.length; index += 1) {
+     phrase = string
+       phrase.includes('x') 
+         ph = string
+         let res = `Tryber ${ph} aqui`  
+          
+      
+       
+   
+     
+  }
+    
 }
+  console.log(changeString('Carlos'))
