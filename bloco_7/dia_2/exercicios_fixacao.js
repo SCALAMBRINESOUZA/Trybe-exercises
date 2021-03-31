2d475cd27374e7cdbef8f7d167306e493c6dd30f
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-31 10:09:42 -0300

    Segundo exemplo exibindo as propriedades com for in

--- bloco_7/dia_2/exercicios_fixacao.js
+++ bloco_7/dia_2/exercicios_fixacao.js
@@ -48,29 +48,72 @@
 //   customer[newKey] = fullName;
 //   console.log(customer);
 
-const objeto = {
-    name: 'carlos',
-    age: 34,
-    city: 'sp'
-};
+// const objeto = {
+//     name: 'carlos',
+//     age: 34,
+//     city: 'sp'
+// };
     
-const person = (objeto, key, value) => {
-    let newKey = key;
-    key = value;
-    objeto[newKey] = key;        
-    return objeto
-}
-    console.log(person(objeto, 'cor', 'negro'))
+// const person = (objeto, key, value) => {
+//     let newKey = key;
+//     key = value;
+//     objeto[newKey] = key;        
+//     return objeto
+// }
+//     console.log(person(objeto, 'cor', 'negro'))
     
 
+// const student1 = {
+//     Html: 'Muito Bom',
+//     Css: 'Bom',
+//     JavaScript: 'Ótimo',
+//     SoftSkills: 'Ótimo',
+//   };
+    
+//  const student2 = {
+//     Html: 'Bom',
+//     Css: 'Ótimo',
+//     JavaScript: 'Ruim',
+//     SoftSkills: 'Ótimo',
+//     Git: 'Bom', // chave adicionada
+//   };
+  
+//   const listSkills = (student) => {
+//     const arrayOfSkills = Object.keys(student);
+//     for(index in arrayOfSkills){
+//       console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+//     }
+//   };
+
+//   console.log('Estudante 1');
+//   listSkills(student1)
+//   console.log("")
+//   console.log('Estudante 2');
+//   listSkills(student2)
 
 
-
-
-
-
-
-
-
+const student = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkill: 'Ótimo',
+  };
+  
+//   const listSkillsWithFor = (student) => {
+//     const skills = [];
+//     for(skill in student) {
+//       skills.push(student[skill]);
+//     }
+  
+//     return skills;
+//   };
+  
+  const listSkillsWithValues = (student) => Object.values(student);
+  
+  // Sem Object.values
+  //console.log(listSkillsWithFor(student));
+  
+  // Com Object.values
+ console.log(listSkillsWithValues(student));
 
 
