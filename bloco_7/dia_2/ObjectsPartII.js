1f4deac55f3c01ab916c414f654ad07cc92d6ff5
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-04-04 02:42:20 -0300

    Primeiro bonus selecionando quantos alunos assistiram a aula de matematica

--- bloco_7/dia_2/ObjectsPartII.js
+++ bloco_7/dia_2/ObjectsPartII.js
@@ -89,3 +89,14 @@ const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
 //   console.log(verifyPair(lesson2,'professor','Carlos'));
 //////////////////////////////////////////////////////////////////////////////
 
+const getNumberOfStudentsMath = (obj) => {
+    let total = 0;
+    const array = Object.keys(obj);
+    for (index in array) {
+      if(obj[array[index]].materia === 'Matemática'){
+      total += obj[array[index]].numeroEstudantes;
+      }
+    }
+    return total;
+  }
+  console.log(getNumberOfStudentsMath(allLessons)); 
