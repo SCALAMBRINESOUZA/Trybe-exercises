5dd2039b65cc56a0be1c88ecce83f129a58ffe6b
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-04-04 00:52:54 -0300

    add chave no objeto lesson2

---
+++ bloco_7/dia_2/ObjectsPartII.js
@@ -0,0 +1,28 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+  const includeMorning = (obj, key, value) => {
+    let keynew = key;
+    key = value;
+    obj[keynew] = key;
+    return obj;
+  }
+  includeMorning(lesson2, 'turno', 'manha');
+  console.log(lesson2)
