fed36009a8738d578123e7c7e9487edb67f88162
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-04-04 02:50:43 -0300

    Segundo bonus retornando um objeto em formato de relatorio exemplo com console.table

--- bloco_7/dia_2/ObjectsPartII.js
+++ bloco_7/dia_2/ObjectsPartII.js
@@ -102,24 +102,24 @@ const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
 //   console.log(getNumberOfStudentsMath(allLessons)); 
 /////////////////////////////////////////////////////////////////////////////////
 
-// const getInfo = (obj, name) => {
-//     const allLessons = [];
-//     let allStudent = 0;
-//     const array = Object.values(obj);
-//     for (index in array) {
-//       if (array[index].professor === name) {
-//         allLessons.push(array[index].materia)
-//         allStudent += array[index].numeroEstudantes;
-//       }
-//     }
-//     return { lessons: allLessons, estudantes: allStudent };
-//   }
+const getInfo = (obj, name) => {
+    const allLessons = [];
+    let allStudent = 0;
+    const array = Object.values(obj);
+    for (index in array) {
+      if (array[index].professor === name) {
+        allLessons.push(array[index].materia)
+        allStudent += array[index].numeroEstudantes;
+      }
+    }
+    return { lessons: allLessons, estudantes: allStudent };
+  }
   
-//   const createReport = (allLessons, name) => {
-//     const report = {};
-//     report.professor = name;
-//     Object.assign(report, getInfo(allLessons, name));
-//     return report;
-//   }
-//   console.log(createReport(allLessons, 'Carlos'));
-///////////////////////////////////////////////////////////////////////
+  const createReport = (allLessons, name) => {
+    const report = {};
+    report.professor = name;
+    Object.assign(report, getInfo(allLessons, name));
+    return report;
+  }
+  console.table(createReport(allLessons, 'Carlos'));
+
