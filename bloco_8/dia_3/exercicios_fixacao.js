39bebc9874629ffe37030a727b113a87551af360
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-04-05 15:28:49 -0300

    quarto exemplo usando metodo map

--- bloco_8/dia_3/exercicios_fixacao.js
+++ bloco_8/dia_3/exercicios_fixacao.js
@@ -57,22 +57,46 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+// const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
 
-const prices = [2.99, 3.99, 1.5, 2];
+// const prices = [2.99, 3.99, 1.5, 2];
 
-const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
-    { [product]: listPrices[index] }
-  ));
+// const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+//     { [product]: listPrices[index] }
+//   ));
 
-const listProducts = updateProducts(products, prices);
-console.log(listProducts);
+// const listProducts = updateProducts(products, prices);
+// console.log(listProducts);
 // => [
 //   { Arroz: 2.99 },
 //   { Feijao: 3.99 },
 //   { Alface: 1.5 },
 //   { Tomate: 2 },
 // ]
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const numeros = [1, 2, 3, 4, 5, 6];
+console.log(numeros.map((numero) => numero * 2)); // Retorno: [2, 4, 6, 8, 10, 12]
+
+const paresMenoresQueCinco = [];
+numeros.forEach((numero) => {
+  if (numero < 5 && numero % 2 === 0) {
+    paresMenoresQueCinco.push(numero);
+  }
+});
+console.log(paresMenoresQueCinco); // Retorno: [2, 4]
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
 
 
 
