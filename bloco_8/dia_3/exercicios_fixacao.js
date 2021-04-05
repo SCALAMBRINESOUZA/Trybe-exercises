a732d091eeb6506932560a8bf0044603d918ceee
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-04-05 15:27:38 -0300

    terceiro exemplo usando metodo map

--- bloco_8/dia_3/exercicios_fixacao.js
+++ bloco_8/dia_3/exercicios_fixacao.js
@@ -46,14 +46,37 @@
 //   const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
   
 //   console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// const numbers = [1, 2, 3, 4, -5];
+
+// const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+
+// console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+// console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-const numbers = [1, 2, 3, 4, -5];
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
 
-const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+const prices = [2.99, 3.99, 1.5, 2];
 
-console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
-console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+    { [product]: listPrices[index] }
+  ));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ]
+
+
+
+
 
 
 
