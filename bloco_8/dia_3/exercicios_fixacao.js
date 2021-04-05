507e116b814237ba401e13d60816dff3435e70b1
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-04-05 15:24:56 -0300

    segundo exemplo usando metodo map

--- bloco_8/dia_3/exercicios_fixacao.js
+++ bloco_8/dia_3/exercicios_fixacao.js
@@ -37,17 +37,23 @@
 // console.log(newListStudents); // [ 'Maria', 'Manuela', 'Jorge', 'Wilson' ]
 //////////////////////////////////////////////////////////////////////////////
 
-const persons = [
-    { firstName: 'Maria', lastName: 'Ferreira' },
-    { firstName: 'João', lastName: 'Silva' },
-    { firstName: 'Antonio', lastName: 'Cabral' },
-  ];
+// const persons = [
+//     { firstName: 'Maria', lastName: 'Ferreira' },
+//     { firstName: 'João', lastName: 'Silva' },
+//     { firstName: 'Antonio', lastName: 'Cabral' },
+//   ];
   
-  const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+//   const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
   
-  console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
+//   console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
 
 
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
 
 
 
