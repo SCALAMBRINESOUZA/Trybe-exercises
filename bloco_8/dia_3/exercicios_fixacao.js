89c2d992f9b8c99a032f46a6e17365df4b57b999
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-04-05 15:32:35 -0300

    quinto exemplo usando metodo map

--- bloco_8/dia_3/exercicios_fixacao.js
+++ bloco_8/dia_3/exercicios_fixacao.js
@@ -75,17 +75,141 @@
 // ]
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const numeros = [1, 2, 3, 4, 5, 6];
-console.log(numeros.map((numero) => numero * 2)); // Retorno: [2, 4, 6, 8, 10, 12]
+// const numeros = [1, 2, 3, 4, 5, 6];
+// console.log(numeros.map((numero) => numero * 2)); // Retorno: [2, 4, 6, 8, 10, 12]
 
-const paresMenoresQueCinco = [];
-numeros.forEach((numero) => {
-  if (numero < 5 && numero % 2 === 0) {
-    paresMenoresQueCinco.push(numero);
-  }
-});
-console.log(paresMenoresQueCinco); // Retorno: [2, 4]
+// const paresMenoresQueCinco = [];
+// numeros.forEach((numero) => {
+//   if (numero < 5 && numero % 2 === 0) {
+//     paresMenoresQueCinco.push(numero);
+//   }
+// });
+//console.log(paresMenoresQueCinco); // Retorno: [2, 4]
 /////////////////////////////////////////////////////////////////////////////////////////////
+
+// const estudantes = [
+//     {
+//       nome: 'Jorge',
+//       sobrenome: 'Silva',
+//       idade: 14,
+//       turno: 'Manhã',
+//       materias: [
+//         { name: 'Matemática', nota: 67 },
+//         { name: 'Português', nota: 79 },
+//         { name: 'Química', nota: 70 },
+//         { name: 'Biologia', nota: 65 },
+//       ],
+//     },
+//     {
+//       nome: 'Mario',
+//       sobrenome: 'Ferreira',
+//       idade: 15,
+//       turno: 'Tarde',
+//       materias: [
+//         { name: 'Matemática', nota: '59' },
+//         { name: 'Português', nota: '80' },
+//         { name: 'Química', nota: '78' },
+//         { name: 'Biologia', nota: '92' },
+//       ],
+//     },
+//     {
+//       nome: 'Jorge',
+//       sobrenome: 'Santos',
+//       idade: 15,
+//       turno: 'Manhã',
+//       materias: [
+//         { name: 'Matemática', nota: '76' },
+//         { name: 'Português', nota: '90' },
+//         { name: 'Química', nota: '70' },
+//         { name: 'Biologia', nota: '80' },
+//       ],
+//     },
+//     {
+//       nome: 'Maria',
+//       sobrenome: 'Silveira',
+//       idade: 14,
+//       turno: 'Manhã',
+//       materias: [
+//         { name: 'Matemática', nota: '91' },
+//         { name: 'Português', nota: '85' },
+//         { name: 'Química', nota: '92' },
+//         { name: 'Biologia', nota: '90' },
+//       ],
+//     },
+//     {
+//       nome: 'Natalia',
+//       sobrenome: 'Castro',
+//       idade: 14,
+//       turno: 'Manhã',
+//       materias: [
+//         { name: 'Matemática', nota: '70' },
+//         { name: 'Português', nota: '70' },
+//         { name: 'Química', nota: '60' },
+//         { name: 'Biologia', nota: '50' },
+//       ],
+//     },
+//     {
+//       nome: 'Wilson',
+//       sobrenome: 'Martins',
+//       idade: 14,
+//       turno: 'Manhã',
+//       materias: [
+//         { name: 'Matemática', nota: '80' },
+//         { name: 'Português', nota: '82' },
+//         { name: 'Química', nota: '79' },
+//         { name: 'Biologia', nota: '75' },
+//       ],
+//     },
+//   ];
+
+  // buscar um estudante //
+
+//   const allNameStudents = estudantes.filter((estudante) => (
+//     estudante.turno === 'Manhã')).map((estudante) => `${estudante.nome} ${estudante.sobrenome}`);
+  
+//   console.log(allNameStudents);
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// encontrar um estudante
+  
+// const reportStatus = (name, students) => {
+//     const studentInfo = students.find((student) => student.nome === name);
+//     return studentInfo.materias.map((materia) => (
+//       `${materia.name} ${(materia.nota >= 60) ? 'Aprovado' : 'Reprovado'}`
+//     ));
+//   };
+  
+//   console.log(reportStatus('Natalia', estudantes));
+///////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
