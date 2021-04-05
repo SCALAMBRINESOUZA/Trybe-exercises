121f7a27e06c484211123adc97e52d4cb87c75fe
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-04-05 15:09:18 -0300

    terceiro exemplo usando metodo filter

--- bloco_8/dia_3/exercicios_fixacao.js
+++ bloco_8/dia_3/exercicios_fixacao.js
@@ -12,16 +12,35 @@
 // console.log(isEven2); // [ 30, 22 ]
 /////////////////////////////////////////////////////////////////////////////////
 
-const objPeople = [
-    { name: 'José', age: 21 },
-    { name: 'Lucas', age: 19 },
-    { name: 'Maria', age: 16 },
-    { name: 'Gilberto', age: 18 },
-    { name: 'Vitor', age: 15 },
-  ];
+// const objPeople = [
+//     { name: 'José', age: 21 },
+//     { name: 'Lucas', age: 19 },
+//     { name: 'Maria', age: 16 },
+//     { name: 'Gilberto', age: 18 },
+//     { name: 'Vitor', age: 15 },
+//   ];
   
-  const verifyAgeDrive = (arrayOfPeople) => (
-    arrayOfPeople.filter((people) => (people.age < 18))
-  );
+//   const verifyAgeDrive = (arrayOfPeople) => (
+//     arrayOfPeople.filter((people) => (people.age < 18))
+//   );
   
-  console.log(verifyAgeDrive(objPeople)); // [ { name: 'Maria', age: 16 }, { name: 'Vitor', age: 15 } ]
+//   console.log(verifyAgeDrive(objPeople)); // [ { name: 'Maria', age: 16 }, { name: 'Vitor', age: 15 } ]
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+const arrayMyStudents = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const removeStudentByName = (name, listStudents) =>
+   listStudents.filter((student) => student !== name); // Filtra todos os estudantes que não têm o nome 'Ricardo' e retorna um array com eles. Na prática, remove o Ricardo do array.
+
+const newListStudents = removeStudentByName('Ricardo', arrayMyStudents);
+console.log(newListStudents); // [ 'Maria', 'Manuela', 'Jorge', 'Wilson' ]
+
+
+
+
+
+
+
+
+
