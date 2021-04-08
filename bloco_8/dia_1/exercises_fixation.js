a33f96a791bc587cbe63220682b1257c2c447084
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-04-08 00:54:50 -0300

    Segundo exemplo

--- bloco_8/dia_1/exercises_fixation.js
+++ bloco_8/dia_1/exercises_fixation.js
@@ -1,22 +1,38 @@
-// const greaterThan = (firstNumber) => (secondNumber) => secondNumber > firstNumber;
+// // const greaterThan = (firstNumber) => (secondNumber) => secondNumber > firstNumber;
 
-// //const greaterThan10 = greaterThan(10);
-// console.log(greaterThan(15)(16));
-// // true
-//////////////////////////////////////////////////////////////////////////////////////////////
+// // //const greaterThan10 = greaterThan(10);
+// // console.log(greaterThan(15)(16));
+// // // true
+// //////////////////////////////////////////////////////////////////////////////////////////////
 
-const wakeUp = () => console.log('acordando');
+// const wakeUp = () => console.log('acordando');
 
+// const breakFast = () => console.log('Bora tomar café!!');
 
-const breakFast = () => console.log('Bora tomar café!!');
+// const goSleep = () => console.log('Partiu dormir!!');
 
-const goSleep = () => console.log('Partiu dormir!!');
-
-const doingThings = ((tasks) => {
-  tasks = wakeUp();
-  tasks = breakFast();
-  tasks = goSleep();
+// const doingThings = (functions) => {
   
-});
+//     return functions();
+  
+// };
 
-doingThings(wakeUp);
+// doingThings(wakeUp);
+
+const sum = (a, b) => console.log(a + b);
+
+const sub = (a, b) => console.log(a - b);
+
+const div = (a, b) => console.log(a / b);
+
+const res = (a, b) => console.log(a % b == 0);
+    
+const calculator = (oper, num1, num2) => {
+    return oper(num1, num2); 
+}
+    calculator(sum, 34, 12);
+    calculator(sub, 34, 12);
+    calculator(div, 34, 12);
+    calculator(res, 36, 12)
+
+
