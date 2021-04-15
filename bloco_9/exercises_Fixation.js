e4faba5cfb0bf8cbced1f38a394ebd5794d7044b
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-04-14 21:23:25 -0300

    update exercises

--- bloco_9/exercises_Fixation.js
+++ bloco_9/exercises_Fixation.js
@@ -63,37 +63,74 @@
             // conceitos de assincronicidade usando a função setTimeOut  //
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
-const myExpenses = [
-    {
-      gym: 99,
-    },
-    {
-      ifood: 200,
-    },
-    {
-      phone: 60,
-    },
-    {
-      internet: 100,
-    },
-  ];
+     
   
-  const myIncome = 1000;
-
-  const monthlyBudget = (myIncome, myExpenses, callback) => {
-
-    const totalExpenses = callback(myExpenses);
-    const totalAfterExpenses = myIncome - totalExpenses;
+// const myExpenses = [
+//     {
+//       gym: 99,
+//     },
+//     {
+//       ifood: 200,
+//     },
+//     {
+//       phone: 60,
+//     },
+//     {
+//       internet: 100,
+//     },
+//   ];
   
-    console.log(`Balanço do mês:
-      Recebido: R$${myIncome},00
-      Gasto: R$${totalExpenses},00
-      Saldo: R$${totalAfterExpenses},00 `);
-  };
+//   const myIncome = 1000;
 
-monthlyBudget(myIncome, myExpenses, totalExpenses);
+//   const monthlyBudget = (myIncome, myExpenses, callback) => {
+
+//     const totalExpenses = callback(myExpenses);
+//     const totalAfterExpenses = myIncome - totalExpenses;
+  
+//     console.log(`Balanço do mês:
+//       Recebido: R$${myIncome},00
+//       Gasto: R$${totalExpenses},00
+//       Saldo: R$${totalAfterExpenses},00 `);
+//   };
 
 
+//   const handleExpenses = myExpenses => {
+//     const eachValue = myExpenses.map((item) => Object.values(item)[0]);
+//     const totalExpense = eachValue.reduce((acc, curr) => acc + curr , 0);
+//     return totalExpense;
+//   };
+  
+//   monthlyBudget(myIncome, myExpenses, handleExpenses);
 
 
+// const joinName = ((fullName) => {
+//     console.log(fullName);
+// })
 
+// const FlName = (FirstName, lastName) => `${FirstName} ${lastName}`;
+
+// let names = FlName('carlos', 'henrique')
+
+// joinName(names);
+///////////////////////////////////////////////////////////////////////////////////
+
+const assert = require('assert');
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = (otherFunct) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian",
+    };
+    return otherFunct(userToReturn  );
+};
+
+const showName = Map.getUser((user) => `Hello My name is ${user.userToReturn.firstName} ${user.userToReturn.lastName}`);
+
+getUser(showName);
+
+assert.strictEqual(getUser(), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
+// assert.strictEqual(getUser(), "Ivan is Russian"); // complete a chamada da função de getUser
