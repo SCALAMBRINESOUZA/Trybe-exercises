a5946ad6b0950c5444fb58227751c7fdf2a36191
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-05-02 20:12:28 -0300

    last exercise done

---
+++ Modulo-Front-End/dia_1/my_first_app/src/components/Component.jsx
@@ -0,0 +1,14 @@
+// import React from  'react';
+
+// class Comp extends React.Component {
+//     render() {
+//         return (
+//             <main>
+//                 <h1>Carlos Henrique Scalambrine de Souza</h1>
+//                 <p>Dedicação e comprometimento estruturam meus pilares</p>
+//             </main>
+//         )
+//     }
+// }
+
+// export default Comp;
