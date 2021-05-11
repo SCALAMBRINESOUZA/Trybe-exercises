a736e4110c432cca94c7ce724278da76bd18c37f
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-05-11 16:37:26 -0300

    exercise pokedex finished

--- Modulo-Front-End/dia_2/second-app/src/App.js
+++ Modulo-Front-End/dia_2/second-app/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import React, {Component} from 'react';
-import Props from './Props';
+import Pokedex from './Pokedex';
+import pokemons from './data';
+
 
 class App extends Component {
   render() {
     return (
       <section>
-        <Props name='carlos' lastName='Henrique' />
+        <section className="App">
+          <h1> Pokedex </h1>
+          <Pokedex pokemons={pokemons} />
+        </section>
       </section>
    );
   }
