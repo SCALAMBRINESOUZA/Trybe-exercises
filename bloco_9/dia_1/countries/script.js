424844480676b5a118766a34314c287e066817db
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-05-16 00:00:24 -0300

    finished block nine. one

--- bloco_9/dia_1/countries/script.js
+++ bloco_9/dia_1/countries/script.js
@@ -1,27 +1,38 @@
 const renderCountry = (country) => {
   listOrder = document.createElement('ul');
-  document.body.appendChild(listOrder)
   itemList = document.createElement('li')
+  const span = document.createElement('span')
+  const img = document.createElement('img')
+  
+  img.setAttribute('class', 'image')
+  img.src = country.flag
+  document.body.appendChild(listOrder)
+  span.innerHTML = `${country.nativeName} - ${country.capital}`  
+  
+  itemList.appendChild(img)
+  itemList.appendChild(span)
   listOrder.appendChild(itemList);
-  itemList.innerHTML = `${country.nativeName} - ${country.capital}`  
-
+    
 }
 
-const fetchCountry = names => {
-    fetch(`https://restcountries.eu/rest/v2/name/${names}`)
-        .then((response) => {
-            response.json()
-                .then((countries) => {
-                   renderCountry(countries[0]) 
-                fetch('https://restcountries.eu/rest/v2/name/brazil')
-                .then((response) => {
-                    response.json()
-                        .then((countries) => {
-                           renderCountry(countries[0]) 
-                })
-            })
-        })        
-    });
+
+const myFetch = (names) => fetch(`https://restcountries.eu/rest/v2/name/${names}`)
+    .then((response) => response.json())
+      .then((countries) => {
+            renderCountry(countries[0])
+      })
+
+
+
+const fetchCountry = async () => {
+    try{
+    await myFetch('Brazil')
+    await myFetch('Angola')
+    await myFetch('Nigeria')
+    await myFetch('Fiji')
+    await myFetch('Cuba')               
+    } catch(_error) { alert('Ocorreu um erro ao buscar pais')}
 }
 
-window.onload = fetchCountry('Germany');
+
+window.onload = fetchCountry();
