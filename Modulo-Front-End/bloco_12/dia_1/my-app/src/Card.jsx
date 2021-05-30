7abdc3933bfef67d9f13b63290c1123a81d309fa
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-05-30 16:57:55 -0300

    update

--- Modulo-Front-End/bloco_12/dia_1/my-app/src/Card.jsx
+++ Modulo-Front-End/bloco_12/dia_1/my-app/src/Card.jsx
@@ -4,8 +4,9 @@ class Card extends React.Component {
     render() {
         return (
             <div>
+              <h1>{this.props.title}</h1>  
               <img src={this.props.imgSrc} alt={this.props.altImg}/>
-              <h1>{this.props.description}</h1>
+              <h2>{this.props.description}</h2>
               <p>{this.props.meme}</p>      
                
             </div>
