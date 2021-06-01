59f8ad92b437ef61b971de30d8211c1d0c59f874
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-06-01 00:15:33 -0300

    update

--- bloco_4/dia_3/exercicios_fixacao/firstExample.js
+++ bloco_4/dia_3/exercicios_fixacao/firstExample.js
@@ -12,17 +12,18 @@
 //  console.table(`A jogadora ${player.fullName} foi eleita a melhor do mundo nos anos de ${player.bestInTheWorld}`)
 // console.log(`A jogadora possui ${player.medals.golden} medalhas de ouro e ${player.medals.silver} medalhas de prata `)
 
-// const singer = {
-// 	name: 'Milton',
-// 	lastName: 'Nascimento',
-// 	nickName: 'Bituca',
-// 	age: 77,
-// 	bestAlbuns: ['Travessia', 'Clube da Esquina', 'Minas'],
-// 	bornInfo: {city: 'Minas Gerais', Bairro: 'Governador Valadares'}
-// }
+const singer = {
+	name: 'Milton',
+	lastName: 'Nascimento',
+	nickName: 'Bituca',
+	age: 77,
+	bestAlbuns: ['Travessia', ' Clube da Esquina', ' Minas'],
+	bornInfo: {city: 'Minas Gerais', Bairro: 'Governador Valadares'}
+}
+				// exemplos de desestruturação de arrays e objetos //
 
+// const [,  bst] = singer.bestAlbuns 
+// const {name, lastName, age, nickName, bornInfo: {Bairro}, } = singer
+//  console.log(`O cantor ${name} ${lastName} tem ${age} anos de idade
+//  tambem conhecido pelo apelido de ${nickName} reside no bairro de ${Bairro} sendo o melhor album dentre eles o${bst}`)
 
-// console.log(`O cantor ${singer.name} ${singer.lastName} tem ${singer.age} anos de idade
-// tambem conhecido pelo apelido de ${singer.nickName}`)
-
-// console.table(singer.bornInfo.Bairro)
