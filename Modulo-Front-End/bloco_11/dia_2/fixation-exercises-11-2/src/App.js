1501d349b95fa99c6863b84a0825f2796d6b7370
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-06-02 19:59:29 -0300

    update

--- Modulo-Front-End/bloco_11/dia_2/fixation-exercises-11-2/src/App.js
+++ Modulo-Front-End/bloco_11/dia_2/fixation-exercises-11-2/src/App.js
@@ -1,144 +1,145 @@
-import './App.css';
-//import Image from './Image'
-import React, { Component } from 'react';
-import Order from './Order';
-//import Album from './Album';
-//import UserProfile from './UserProfile';
-//import './Image.css'
+// import './App.css';
+// //import PropTypes from 'prop-types'
+// //import Image from './Image'
+// import React, { Component } from 'react';
+// //import Order from './Order';
+// //import Album from './Album';
+// //import UserProfile from './UserProfile';
+// //import './Image.css'
 
 
-// function Greeting(props) {
-//   return (<h1>Hello, {props.name} {props.lastName}</h1>)
-// }
+// // function Greeting(props) {
+// //   return (<h1>Hello, {props.name} {props.lastName}</h1>)
+// // }
 
-// class Greetings extends Component {
+// // class Greetings extends Component {
+// //   render() {
+// //     return(
+// //       <h1>Hello, {this.props.name} como vai com seus {this.props.age } anos seu nome completo é {this.props.fullname}</h1>
+// //     )
+// //   }
+// // }
+
+// //   Greetings.propTypes = {
+// //     name: PropTypes.string.isRequired,
+// //     age: PropTypes.number.isRequired,
+// //     fullname: PropTypes.arrayOf(PropTypes.string.isRequired)
+// //   }
+
+// class App extends Component {
+
 //   render() {
-//     return(
-//       <h1>Hello, {this.props.name} {this.props.lastName}</h1>
-//     )
+    
+//     // const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
+//     // const item = shoppingList.map((item, index) => (<li key={index}> {item} </li>))
+    
+  
+//     // const headphone = {
+//     //   id: 102,
+//     //   user: "cena@gmail.com",
+//     //   product: "Razer Headphone",
+//     //   price: {
+//     //     value: 99.99,
+//     //     currency: "dollars"
+//     //   }
+//     // };
+
+//     // const energyDrink = {
+//     //   id: 77,
+//     //   user: "cena@gmail.com",
+//     //   product: "Monster 500mL",
+//     //   price: {
+//     //     value: 9.99,
+//     //     currency: "dollars"
+//     //   }
+//     // };
+
+
+//   //   const users = [  
+//   //     {
+//   //     id: 8,
+//   //     name: "Pedro",
+//   //     email: "pedro@gmail.com",
+//   //     avatar: "https:cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+//   //     },
+         
+   
+//   //   {
+       
+//   //     id: 3,
+//   //     name: "Sarah",
+//   //     email: "sarah@gmail.com",
+//   //     avatar: "https:cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+//   //   },
+
+//   //   {   
+     
+//   //     id: 13,
+//   //     name: "Joao",
+//   //     email: "joao@gmail.com",
+//   //     avatar: "https:cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+//   //   },
+//   // ]
+    
+//     //  const album01 = {
+//     //   image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/0/0b/Direto_do_Campo_de_Exterm%C3%ADnio.jpg/220px-Direto_do_Campo_de_Exterm%C3%ADnio.jpg',
+//     //   title: 'Direto do campo de exterminio',
+//     //   description: 'O grupo tem um estilo musical agressivo, violento, as letras do grupo seguem um violento estilo, entretanto racional. O grupo utiliza a linguagem da periferia (gírias) e a linguagem formal. Também é comum utilizar partes de músicas clássicas para iniciarem suas músicas. A religião se faz presente como mediadora, uma metáfora para a violência da Terra, como em "Hoje Deus Anda de Blindado" (Uma alusão à música de 1999 do grupo Pavilhão 9, "Se Deus Vier, Que Venha Armado").',
+//     //   releaseDate: {
+//     //     year: '2003',
+//     //     month: '10',
+//     //     day: '24',
+//     //   },
+//     //   others: {
+//     //     recordCompany: 'Sky Blue',
+//     //     formats: 'CD'
+//     //   }
+//     // };
+
+//     // const album02 = {
+//     //   image: 'https://upload.wikimedia.org/wikipedia/pt/2/23/O_Espet%C3%A1culo_do_Circo_dos_Horrores.jpg',
+//     //   title: 'O Espetáculo do Circo dos Horrores ',
+//     //   description: 'Após a censura do videoclipe do grupo no disco Versos Sangrentos, o Facção Central lançou o álbum A Marcha Fúnebre Prossegue, que inicia-se com uma introdução à notícia da censura, dada em vários telejornais com os dizeres "Rap que faz apologia ao crime, Facção Central", divulgado no Jornal Nacional por Fátima Bernardes. Essa introdução é composta por vários "recortes" de noticiários da televisão brasileira. Após a faixa "Introdução", vem em seguida a faixa "Dia Comum"' ,
+//     //   releaseDate: {
+//     //     year: '2006',
+//     //     month: '06',
+//     //     day: '06',
+//     //   },
+//     //   others: {
+//     //     recordCompany: 'Sky Blue',
+//     //     formats: 'CD, digital'
+//     //   }
+//     // };
+      
+//      return (      
+     
+//     //   // <>
+//       //   <Greeting name='Carlos' lastName='Henrique' />
+//         //  <Greetings name= 'Carlos' age= {34} fullname={['carlos ', 'henrique ', 'scalambrine de ', 'souza']} />
+//       // </>
+//   //  <nav>
+//   //    <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText=' Cute cat staring'/>
+//   //  </nav>
+//     //  <article>
+//     //       <Album album={ album01 } />
+//     //       <Album album={ album02 } />            
+//     //   </article>
+//         // <div className='App'>
+//         //   {users.map((user, key) => <UserProfile key={key} user={user} />)}
+//         // </div>
+//       //  <div className='App'>
+//       //   <h1>Ordes recently created</h1>
+//       //   <Order order={headphone}  />
+//       //   <article className='teste'>
+//       //   <Order order={energyDrink} />
+//       //   </article>
+//       //  </div>
+//         // <div className='div'>
+//         //   <h2 style={{textAlign: 'center', color: 'blue', backgroundColor: 'gray', width: 300, marginLeft: 50,}}>Lista de Compras</h2>
+//         //   <ol className='list'>{item}</ol>
+//         // </div>        
+//     );
 //   }
 // }
 
-
-class App extends Component {
-  render() {
-    // const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
-
-    // const item = shoppingList.map((item, index) => {
-    //   console.log("item:", {item})
-    //   return (<li key={index}> {item} </li>);
-    // });
-  
-    const headphone = {
-      id: 102,
-      user: "cena@gmail.com",
-      product: "Razer Headphone",
-      price: {
-        value: 99.99,
-        currency: "dollars"
-      }
-    };
-
-    const energyDrink = {
-      id: 77,
-      user: "cena@gmail.com",
-      product: "Monster 500mL",
-      price: {
-        value: 9.99,
-        currency: "dollars"
-      }
-    };
-
-
-    // const pedro =  
-    //   {
-    //   id: 8,
-    //   name: "Pedro",
-    //   email: "pedro@gmail.com",
-    //   avatar: "https:cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
-    //   };
-         
-    // const sarah = 
-    // {
-       
-    //   id: 3,
-    //   name: "Sarah",
-    //   email: "sarah@gmail.com",
-    //   avatar: "https:cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
-    // };
-
-    // const joao = 
-    // {   
-     
-    //   id: 13,
-    //   name: "Joao",
-    //   email: "joao@gmail.com",
-    //   avatar: "https:cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
-    // };
-    
-    //  const album01 = {
-    //   image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/0/0b/Direto_do_Campo_de_Exterm%C3%ADnio.jpg/220px-Direto_do_Campo_de_Exterm%C3%ADnio.jpg',
-    //   title: 'Direto do campo de exterminio',
-    //   description: 'O grupo tem um estilo musical agressivo, violento, as letras do grupo seguem um violento estilo, entretanto racional. O grupo utiliza a linguagem da periferia (gírias) e a linguagem formal. Também é comum utilizar partes de músicas clássicas para iniciarem suas músicas. A religião se faz presente como mediadora, uma metáfora para a violência da Terra, como em "Hoje Deus Anda de Blindado" (Uma alusão à música de 1999 do grupo Pavilhão 9, "Se Deus Vier, Que Venha Armado").',
-    //   releaseDate: {
-    //     year: '2003',
-    //     month: '10',
-    //     day: '24',
-    //   },
-    //   others: {
-    //     recordCompany: 'Sky Blue',
-    //     formats: 'CD'
-    //   }
-    // };
-
-    // const album02 = {
-    //   image: 'https://upload.wikimedia.org/wikipedia/pt/2/23/O_Espet%C3%A1culo_do_Circo_dos_Horrores.jpg',
-    //   title: 'O Espetáculo do Circo dos Horrores ',
-    //   description: 'Após a censura do videoclipe do grupo no disco Versos Sangrentos, o Facção Central lançou o álbum A Marcha Fúnebre Prossegue, que inicia-se com uma introdução à notícia da censura, dada em vários telejornais com os dizeres "Rap que faz apologia ao crime, Facção Central", divulgado no Jornal Nacional por Fátima Bernardes. Essa introdução é composta por vários "recortes" de noticiários da televisão brasileira. Após a faixa "Introdução", vem em seguida a faixa "Dia Comum"' ,
-    //   releaseDate: {
-    //     year: '2006',
-    //     month: '06',
-    //     day: '06',
-    //   },
-    //   others: {
-    //     recordCompany: 'Sky Blue',
-    //     formats: 'CD, digital'
-    //   }
-    // };
-      
-     return (      
-      //  <>
-      //  <UserProfile user={pedro} />
-      //  <UserProfile user={sarah} />
-      //  <UserProfile user={joao} />
-      //  </>
-    //   // <>
-      //   <Greeting name='Carlos' lastName='Henrique' />
-      //   <Greetings name = 'Scalambrine' lastName='Souza'/>
-      // </>
-  //  <nav>
-  //    <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText=' Cute cat staring'/>
-  //  </nav>
-    //  <article>
-    //       <Album album={ album01 } />
-    //       <Album album={ album02 } />            
-    //   </article>
-    //   //   <div className='App'>
-      //     {users.map((user, key) => <UserProfile key={key} user={user} />)}
-      //   </div>
-       <div className='App'>
-        <h1>Ordes recently created</h1>
-        <Order order={headphone}  />
-        <article className='teste'>
-        <Order order={energyDrink} />
-        </article>
-       </div>
-        // <div>
-        //   <h2 style={{textAlign: 'center'}}>Lista de Compras</h2>
-        //   <ol className='list'>{item}</ol>
-        // </div>
-    );
-  }
-}
-
-export default App;
+// export default App;
