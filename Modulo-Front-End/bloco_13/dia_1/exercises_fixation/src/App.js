9f98f9cc955c0a7fc2cabb549504a80c2c97a21c
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-06-06 00:42:21 -0300

    done three examples

--- Modulo-Front-End/bloco_13/dia_1/exercises_fixation/src/App.js
+++ Modulo-Front-End/bloco_13/dia_1/exercises_fixation/src/App.js
@@ -1,13 +1,17 @@
 import { Component } from 'react';
 import './App.css';
+import Counter from './components/Counter';
+// import DadJoke from './components/DadJoke';
+// import MyJokeComponent from './components/MyJokeComponent';
 
 class App extends Component {
   render() {
     return (
-      <span>Hello, world</span>
+      // <MyJokeComponent />
+      // <DadJoke />
+      <Counter />
     )
   }
   
 }
-
 export default App;
