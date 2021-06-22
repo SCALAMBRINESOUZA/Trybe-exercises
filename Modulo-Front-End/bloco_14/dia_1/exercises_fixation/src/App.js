5d7197dd1676f232e2d5b2d988b1bb383f30029a
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-06-22 01:27:47 -0300

    examples of the block one incomplete

--- Modulo-Front-End/bloco_14/dia_1/exercises_fixation/src/App.js
+++ Modulo-Front-End/bloco_14/dia_1/exercises_fixation/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react'
+// import Teste from './Teste'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// Todas as linhas comentadas se referem a estudos de passagem de props e states vinculado com arquivo Teste.js //
+
+class App extends React.Component {
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     n1: ''
+  //   }
+  //   this.vamosLa = this.vamosLa.bind(this)
+  //   this.somar = this.somar.bind(this)
+
+  // }
+
+  // somar(n, m) {
+  //   return `A soma de ${n} + ${m} = ${parseInt(n) + parseInt(m)}`
+  // }
+
+  // componentDidMount() {this.vamosLa()}
+  // vamosLa() {
+  //   this.setState({
+  //     n1: '25'
+  //   })
+  // }
+
+  render() { 
+    // const { n1 } = this.state
+    return (
+      <div className="App">
+      <p>Learn React</p>
+      {/* <Teste n2= '25' n1={ n1 } somar={ this.somar }  /> */}
+      <label>
+      E-mail
+        <input type="email" />
+        <input style={{marginTop: '30px', position: 'absolute', width: '70px'}} 
+        id="btn-send" 
+        type="button" 
+        value="Enviar" />
+      </label>
+      <button 
+      data-testid='testinho' 
+      value='btnEnviar'
+      >
+        Adicionar
+      </button>
+
+     </div>
   );
+}
 }
 
 export default App;
