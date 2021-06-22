3ec83a60d1cb1c784bd29ac9b60c90f9324042bc
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-06-22 01:29:46 -0300

    created a file to study the passage of props

---
+++ Modulo-Front-End/bloco_14/dia_1/exercises_fixation/src/Teste.js
@@ -0,0 +1,16 @@
+// import React from 'react'
+// class Teste extends React.Component {
+//     render() {
+//         const { n1, n2, somar } = this.props
+      
+//         return (
+//             <main>
+//                 <p> Primeiro numero { n1 }</p>
+//                 <p> Segundo numero { n2 }</p>
+//                 <h1>{ somar(n1, n2) }</h1>
+//             </main>
+
+//         )
+//     }
+// }
+// export default Teste;
