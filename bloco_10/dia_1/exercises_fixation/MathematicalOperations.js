1076c7c42c43616f234316a9d04986ba0400295c
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-07-02 17:24:55 -0300

    update

---
+++ bloco_10/dia_1/exercises_fixation/MathematicalOperations.js
@@ -0,0 +1,14 @@
+const sum = (a, b) => a + b;
+
+const multiply = (a, b) => a * b;
+
+const division = (a, b) => a / b;
+
+const subtraction = (a, b ) => a - b;
+
+module.exports = {
+    sum,
+    multiply,
+    division,
+    subtraction,
+};
