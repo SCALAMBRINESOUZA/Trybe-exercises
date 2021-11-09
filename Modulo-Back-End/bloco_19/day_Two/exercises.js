f4b449f3cec47a55fcff4978612f7e5e521b7276
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-11-09 15:59:00 -0300

    query to show sum of number ten and eleven

--- Modulo-Back-End/bloco_19/day_Two/exercises.js
+++ Modulo-Back-End/bloco_19/day_Two/exercises.js
@@ -6,3 +6,7 @@
     SELECT 10 AS 'Primeiro Numero', 20 AS 'Segundo Numero', 30 AS 'Terceiro Numero';
 */
 
+/* Escreva uma query para exibir a soma dos números 10 e 15.
+    SELECT(10 + 15) AS 'Valor Somado';
+*/
+
