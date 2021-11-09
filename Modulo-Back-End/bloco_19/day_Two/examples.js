72b40d4975ac44f3ef4b000f19266804ab116706
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-11-09 04:54:46 -0300

    examples of use in the select in situation specify

---
+++ Modulo-Back-End/bloco_19/day_Two/examples.js
@@ -0,0 +1,12 @@
+        /* Rode cada um deles em uma janela de query para verificar os resultados:  */
+/*
+SELECT 'Olá, bem vindo ao SQL!';
+SELECT 10;
+SELECT now();
+SELECT 20 * 2;
+SELECT 50 / 2;
+SELECT 18 AS 'idade';
+SELECT 2021 AS 'Ano';
+SELECT 'Carlos', 'Henrique', 34, 'Desenvolvedor Web'; 
+SELECT 'Carlos' AS 'Nome', 'Henrique' AS 'Sobrenome', 34 AS 'Idade', 'Desenvolvedor Web' AS 'Área de atuação';
+*/
