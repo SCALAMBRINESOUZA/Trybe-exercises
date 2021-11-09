4d0064392b3a30f2f012572e9b646775cc08622b
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-11-09 15:32:20 -0300

    Using all the concepts studied so far

--- Modulo-Back-End/bloco_19/day_Two/examples.js
+++ Modulo-Back-End/bloco_19/day_Two/examples.js
@@ -91,3 +91,48 @@ SELECT 'Carlos' AS 'Nome', 'Henrique' AS 'Sobrenome', 34 AS 'Idade', 'Desenvolve
 /* Quantos emails temos cadastrados nessa tabela?
     SELECT COUNT(email) FROM sakila.staff;
 */
+
+                      // Using all the concepts studied so far
+
+/* Escreva uma query que exiba todos os filmes cadastrados no banco de dados.
+  SELECT * FROM sakila.film;
+*/
+
+/* Escreva uma query que exiba apenas o nome dos filmes, seu ano de lançamento e sua classificação .
+  SELECT title, release_year, rating FROM sakila.film;
+*/
+
+/* Quantos filmes temos cadastrados?
+  SELECT COUNT(*) FROM sakila.film;
+*/
+
+/* Escreva uma query que exiba apenas os sobrenomes únicos cadastrados.
+  SELECT DISTINCT last_name FROM sakila.actor;
+*/
+
+/* Quantos sobrenomes únicos temos na tabela?
+  SELECT COUNT(DISTINCT last_name) FROM sakila.actor;
+*/
+
+/* Ordene os valores na tabela em ordem crescente de sobrenomes e em ordem decrescente de nome.
+  SELECT * FROM sakila.actor
+  ORDER BY first_name DESC, last_name ASC;
+*/
+
+/* Vá até a tabela language do sakila e crie uma pesquisa que mostre os 5 idiomas cadastrados , mas não mostre o idioma english .
+  SELECT name FROM sakila.language LIMIT 5 OFFSET 1;
+*/
+
+/* Agora vamos tentar fazer o seguinte: Crie uma query para encontrar os 20 primeiros filmes, incluindo o título, o ano de lançamento, a duração, a classificação indicativa e o custo de substituição.Ordene os resultados pelos filmes com a maior duração e depois pelo menor custo de substituição.
+  SELECT title, release_year, length, rating, replacement_cost
+  FROM sakila.film ORDER BY length DESC, replacement_cost ASC LIMIT 20;
+*/
+
+
+
+
+
+
+
+
+
