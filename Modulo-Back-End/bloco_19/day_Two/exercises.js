238c291a2357a7e9cceda8de4fb83ffe14ca0984
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-11-09 16:57:02 -0300

    query using concat and count to join string with value name of column

--- Modulo-Back-End/bloco_19/day_Two/exercises.js
+++ Modulo-Back-End/bloco_19/day_Two/exercises.js
@@ -59,7 +59,10 @@
     ORDER BY Hours ASC LIMIT 5;
 */
 
-
+/* Escreva uma query que exiba a string "Existem Number cientistas na tabela Scientists.", em que Number se refira a quantidade de cientistas.
+    SELECT CONCAT('Existem', " ", COUNT(Name),  " ",  'cientistas na tabela Scientists')
+    AS resultado FROM Scientists.Scientists;
+*/
 
 
 
