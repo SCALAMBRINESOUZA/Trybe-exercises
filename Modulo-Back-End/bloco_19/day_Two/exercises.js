6f3d92fdaa68dcf27007089ec7aa76949609c476
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-11-09 16:43:59 -0300

    query to show name of projects that contain the greater quantity hour

--- Modulo-Back-End/bloco_19/day_Two/exercises.js
+++ Modulo-Back-End/bloco_19/day_Two/exercises.js
@@ -44,6 +44,10 @@
     SELECT DISTINCT Project FROM Scientists.AssignedTo;
 */
 
+/* Escreva uma query para exibir o nome do projeto com maior quantidade de horas.
+    SELECT Name FROM Scientists.Projects
+    ORDER BY Hours DESC LIMIT 1;
+*/
 
 
 
