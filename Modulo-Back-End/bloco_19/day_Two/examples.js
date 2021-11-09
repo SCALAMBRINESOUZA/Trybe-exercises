fcd4900351e5aa112168618c771ae1bdf9c31b1f
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-11-09 13:58:59 -0300

    using concat to show title and rating from film table

--- Modulo-Back-End/bloco_19/day_Two/examples.js
+++ Modulo-Back-End/bloco_19/day_Two/examples.js
@@ -58,4 +58,9 @@ SELECT 'Carlos' AS 'Nome', 'Henrique' AS 'Sobrenome', 34 AS 'Idade', 'Desenvolve
 
 /* Na tabela sakila.film , monte uma query que exiba o título e o ano de lançamento dos filmes em uma coluna e dê a ela o nome Lançamento do Filme .
   SELECT CONCAT(title, " ", release_year) AS 'Lançamento do Filme' from sakila.film;
-*/  
+*/
+
+/* Na tabela sakila.film , crie uma query que exiba o título do filme e sua classificação indicativa (PG, G, NC-17) em apenas uma coluna. Dê a ela o nome Classificação . Não se esqueça de deixar um espaço entre as palavras para que fiquem legíveis.
+  SELECT CONCAT(title, " ", rating) AS Classificação from sakila.film;
+*/
+
