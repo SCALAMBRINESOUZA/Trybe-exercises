b91851bbe84035af3d7f239b5518ddb9213f9ca6
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-11-09 15:54:42 -0300

    query to show three number into three columns

--- Modulo-Back-End/bloco_19/day_Two/exercises.js
+++ Modulo-Back-End/bloco_19/day_Two/exercises.js
@@ -1,3 +1,8 @@
 /* Escreva uma query para exibir a string "This is SQL Exercise, Practice and Solution".
-  SELECT ('This is SQL Exercise, Practice and Solution');
+    SELECT ('This is SQL Exercise, Practice and Solution');
 */  
+
+/* Escreva uma query para exibir três números em três colunas.
+    SELECT 10 AS 'Primeiro Numero', 20 AS 'Segundo Numero', 30 AS 'Terceiro Numero';
+*/
+
