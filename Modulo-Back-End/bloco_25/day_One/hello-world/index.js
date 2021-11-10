4f2593a9c78be38720c4a205816c6d91d52d9f22
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-11-10 03:40:09 -0300

    update in the folders

--- Modulo-Back-End/bloco_25/day_One/hello-world/index.js
+++ Modulo-Back-End/bloco_25/day_One/hello-world/index.js
@@ -4,3 +4,5 @@ const name = readLine.question(" Qual é o seu nome? ");
 const age = readLine.questionInt(" Qual é a sua idade? ")
 
 console.log(`hello ${name} welcome the world Nodejs do you have ${age } is staying old he he ! `);
+
+//testando pra subir sem o node_modules
