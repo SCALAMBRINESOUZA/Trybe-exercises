487f47d08929d09514189c6d540feff7b7d63bed
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-11-13 08:01:21 -0300

    exercise bonus about facorial

---
+++ Modulo-Back-End/bloco_25/day_One/Exercises/factorial.js
@@ -0,0 +1,23 @@
+const readLine = require('readline-sync');
+
+const factorial = (x) => {
+  return x <= 0 ? 1 : x * factorial(x - 1)
+}
+
+const resultFactorial = () => {
+  const number = readLine.questionInt('Type here the number to calculate the factorial: ')
+  if(number <= 0 ) {
+  console.log('-------------------------------------------------------------------------');
+      return console.log("The number should be greater than zero") 
+    } 
+  console.log('-------------------------------------------------------------------------');
+     const result = factorial(number)
+     console.log(result);
+}
+resultFactorial(8)
+
+module.exports = resultFactorial;
+
+// var fatorial = function fac(n = readLine.questionInt('Type here the number to calculate the factorial: ')) { return n <= 0 ? 1 : n * fac(n - 1) };
+
+// console.log(fatorial())
--- Modulo-Back-End/bloco_25/day_One/Exercises/index.js
+++ Modulo-Back-End/bloco_25/day_One/Exercises/index.js
@@ -4,8 +4,9 @@ const selectScript = () => {
 
   console.log('Choose the script to be run');
   console.log('1. BMI');
-  console.log('2. average Velocity')
-  console.log('3. prize Draw')
+  console.log('2. average Velocity');
+  console.log('3. prize Draw');
+  console.log('4. factorial')
   console.log('-------------------------------------------------------------------------');
   const scriptSelected = readLine.questionInt('Which script will run? ');
   console.log('-------------------------------------------------------------------------');
@@ -21,7 +22,12 @@ const selectScript = () => {
   } else if (scriptSelected == 3) {
     const prizeDraw = require('./prizeDraw')
     return prizeDraw;
+  
+  } else if (scriptSelected == 4) {
+    const resultFactorial = require('./factorial')
+    return resultFactorial;     
   }
+
 
 } 
 
