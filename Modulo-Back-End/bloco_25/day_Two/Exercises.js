fdf5ff70ad6896cabf92d5e63135b96e00dde149
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-11-16 23:07:40 -0300

    done the listing of all characters

--- Modulo-Back-End/bloco_25/day_Two/Exercises.js
+++ Modulo-Back-End/bloco_25/day_Two/Exercises.js
@@ -1,20 +1,19 @@
-const returnPromisse = (num1, num2, num3) => {
-  return new Promise((resolve, reject) => {
-    if (typeof num1 != "number" || typeof num2 != "number" || typeof num3 != "number") {
-      return reject(new Error('Informe apenas números'))
-    } 
-    const result = (num1 + num2) * num3;
+//  const returnPromisse = (num1, num2, num3) => {
+//   return new Promise((resolve, reject) => {
+//     if (typeof num1 != "number" || typeof num2 != "number" || typeof num3 != "number") {
+//       return reject(new Error('Informe apenas números'))
+//     }
+//     const result = (num1 + num2) * num3;
 
-    if(result < 50) {
-      return reject(new Error('Valor muito baixo'))
-    }
-      resolve(result);
-    
-  });
-}
+//     if(result < 50) {
+//       return reject(new Error('Valor muito baixo'))
+//     }
+//       resolve(result);
 
- 
-returnPromisse(2,3,'d')
-    .then(data => console.log(`Resultado do calculo ${data}`))
-    .catch(err => console.log(` Erro: ${err.message}`))
-  
+//   });
+
+
+// returnPromisse(2,3,'d')
+//     .then(data => console.log(`Resultado do calculo ${data}`))
+//     .catch(err => console.log(` Erro: ${err.message}`))
+
