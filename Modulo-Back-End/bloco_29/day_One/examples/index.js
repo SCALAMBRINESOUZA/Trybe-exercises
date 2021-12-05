a103e14b2bc3cf60073495ad8711b0d6afa250c0
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-05 01:24:10 -0300

    done example one

---
+++ Modulo-Back-End/bloco_29/day_One/examples/index.js
@@ -0,0 +1,83 @@
+/* "Converter" */
+const GRADE_DICT = {
+    0.9: 'A',
+    0.8: 'B',
+    0.7: 'C',
+    0.6: 'D',
+    0.1: 'E',
+};
+
+const gradeKeys = Object.keys(GRADE_DICT);
+
+/* Função menor para remover o uso excessivo de `if{}else`s */
+const getGradeLetter = (gradeNumber) => {
+    let letterGrade = 'F';
+
+    for (let i = 0; i < gradeKeys.length; i += 1) {
+        if (gradeNumber >= gradeKeys[i]) {
+            letterGrade = GRADE_DICT[gradeKeys[i]];
+            break;
+        }
+    }
+
+    return letterGrade;
+};
+
+/* Coletar notas */
+const getLetterGrades = ({ name, grade }) => ({
+    name,
+    grade,
+    letterGrade: getGradeLetter(grade),
+});
+
+/* "Converter" */
+const percentageGradesIntoLetters = ({ name, disciplines, school }) => ({
+    name,
+    school,
+    disciplines: disciplines.map(getLetterGrades),
+});
+
+/* Exemplo de execução */
+// ...
+
+/*
+  Vamos exportar também nossa nova função de `Coletar notas` para testes
+*/
+// module.exports = {
+//   percentageGradesIntoLetters,
+//   approvedStudents,
+//   updateApprovalData,
+//   setApproved,
+/* "Determinar" */
+const approvedStudents = ({ disciplines }) =>
+    disciplines.every(({ grade }) => grade > 0.7);
+
+/* "Atualizar" */
+const updateApprovalData = ({ name: studentName, disciplines }) => {
+    console.log(`A pessoa com nome ${studentName} foi aprovada!`);
+
+    disciplines.map(({ name, letterGrade }) =>
+        console.log(`${name}: ${letterGrade}`));
+};
+
+function setApproved(students) {
+    students
+        .map(percentageGradesIntoLetters)
+        .filter(approvedStudents)
+        .map(updateApprovalData);
+}
+
+/* Exemplo de execução */
+// ...
+
+/*
+  Não se esqueça que é necessário exportar ao final as
+  funções para que você possa testa-las de forma unitária
+*/
+module.exports = {
+    percentageGradesIntoLetters,
+    approvedStudents,
+    updateApprovalData,
+    setApproved,
+    getLetterGrades,
+};
