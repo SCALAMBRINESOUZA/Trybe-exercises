27bae6a7651cff9ec45c44a9625c7f30cc598ddf
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-22 23:11:45 -0300

    create a const and a variable and assigned respective values

--- Modulo-Fundamentos-Web/bloco_4/dia_1/Examples/variables.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_1/Examples/variables.js
@@ -1,21 +1,25 @@
-// Crie uma constante chamada myName e atribua a ela o seu nome(Exemplo: Carolina).
-const myName = "Carlos";
+// // Crie uma constante chamada myName e atribua a ela o seu nome(Exemplo: Carolina).
+// const myName = "Carlos";
 
-// Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
-const birthCity = "São Paulo";
-birthCity = "Bahia"
+// // Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+// const birthCity = "São Paulo";
+// birthCity = "Bahia"
 
-// Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
-let birthYear = 1987;
-birthYear = 2030;
-// Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+// // Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+// let birthYear = 1987;
+// birthYear = 2030;
+// // Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
 
-/* Altere o valor atribuído à constante birthCity . Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔
-R: Estamos tentando alterar um valor de uma const o que não é permitido...
-*/
+// /* Altere o valor atribuído à constante birthCity . Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔
+// R: Estamos tentando alterar um valor de uma const o que não é permitido...
+// */
 
 
-// Utilize o console.log() para imprimir as constantes e variáveis que você criou.
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
+// // Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+// console.log(myName);
+// console.log(birthCity);
+// console.log(birthYear);
+
+// Crie uma costante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
+const base = 5;
+let height = 8;
