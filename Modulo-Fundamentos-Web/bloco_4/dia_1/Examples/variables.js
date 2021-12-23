bfa65b0df155093423106a844e33b07c4ddc83b5
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-22 22:42:02 -0300

    using console to show the values of const and variable

--- Modulo-Fundamentos-Web/bloco_4/dia_1/Examples/variables.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_1/Examples/variables.js
@@ -1,8 +1,13 @@
 // Crie uma constante chamada myName e atribua a ela o seu nome(Exemplo: Carolina).
-let myName = "Carlos";
+const myName = "Carlos";
 
 // Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
-let birthCity = "São Paulo";
+const birthCity = "São Paulo";
 
 // Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
 let birthYear = 1987;
+
+// Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
