9f024f6eea23e148f533326e713c73e69fb50b59
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-22 23:45:33 -0300

    const current hour created

--- Modulo-Fundamentos-Web/bloco_4/dia_1/Examples/operators.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_1/Examples/operators.js
@@ -1,15 +1,18 @@
-// Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100;
-const nota = 80;
+// // Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100;
+// const nota = 80;
 
-/* Implemente uma lógica que verifique se a pessoa candidata foi aprovada, reprovada ou se ela está na lista de espera. Para isso, considere as seguintes informações:
-Se a nota for maior ou igual a 80, imprima "Parabéns, você foi aprovada(o)!"
-Se a nota for menor que 80 e maior ou igual a 60, imprima "Você está na nossa lista de espera"
-Se a nota for menor que 60, imprima "Você foi reprovada(o)"
-*/
-if(nota >= 80) {
-  console.log('Parabéns, você foi aprovada(o)!')
-} else if(nota < 80 && nota >= 60) {
-  console.log('Você está na nossa lista de espera')
-} else {
-  console.log('Você foi reprovada(o)')
-}
+// /* Implemente uma lógica que verifique se a pessoa candidata foi aprovada, reprovada ou se ela está na lista de espera. Para isso, considere as seguintes informações:
+// Se a nota for maior ou igual a 80, imprima "Parabéns, você foi aprovada(o)!"
+// Se a nota for menor que 80 e maior ou igual a 60, imprima "Você está na nossa lista de espera"
+// Se a nota for menor que 60, imprima "Você foi reprovada(o)"
+// */
+// if(nota >= 80) {
+//   console.log('Parabéns, você foi aprovada(o)!');
+// } else if(nota < 80 && nota >= 60) {
+//   console.log('Você está na nossa lista de espera');
+// } else {
+//   console.log('Você foi reprovada(o)');
+// }
+
+// Crie uma constante chamada "currentHour" que recebe um número entre 4 e 24 de sua escolha, para representar as horas do dia.
+const currentHour = 24;
