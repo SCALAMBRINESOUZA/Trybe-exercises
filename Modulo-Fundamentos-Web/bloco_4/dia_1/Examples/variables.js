51c178a66b65ae9dca2f97b4e429653428f4ec3e
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-22 23:14:35 -0300

    create a const area and asigned the multiplication between the base and height

--- Modulo-Fundamentos-Web/bloco_4/dia_1/Examples/variables.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_1/Examples/variables.js
@@ -23,3 +23,7 @@
 // Crie uma costante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
 const base = 5;
 let height = 8;
+
+// Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+const area = base * height;
+console.log(area);
