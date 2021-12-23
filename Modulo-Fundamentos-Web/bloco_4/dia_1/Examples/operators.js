b2292c5523ad0f34bd8def60c7e4a5833e434806
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-23 00:14:02 -0300

    conditional using week day done

--- Modulo-Fundamentos-Web/bloco_4/dia_1/Examples/operators.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_1/Examples/operators.js
@@ -44,4 +44,16 @@ Se o horário estiver entre 4 e 11, insira "Hmmm, cheiro de café recém passado
 // console.log(message);
 
 // Crie uma variável chamada "weekDay" que recebe a string "quarta-feira".
-let weekDay = 'quarta-feira';
+let weekDay = 'domingo';
+
+/*
+Implemente condicionais para que:
+Se nossa variável "weekDay" for "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira" ou "sexta-feira", imprima "Oba, mais um dia de aprendizado na Trybe >:D".
+Se for algum dia de fim de semana, imprima "FINALMENTE, descanso merecido UwU".
+*/
+
+if(weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+  console.log('Oba, mais um dia de aprendizado na Trybe >:D');
+} else if(weekDay === 'sabado' || weekDay === 'domingo') {
+  console.log('FINALMENTE, descanso merecido UwU');
+}
