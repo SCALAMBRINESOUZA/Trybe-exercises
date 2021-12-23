311c5d1fbfb37e3f50f562b7cc2ecd7647ba43b2
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-22 22:38:11 -0300

    add variable birthCity

--- Modulo-Fundamentos-Web/bloco_4/dia_1/Examples/variables.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_1/Examples/variables.js
@@ -1,2 +1,5 @@
 // Crie uma constante chamada myName e atribua a ela o seu nome(Exemplo: Carolina).
-let myName = 'Carlos';
+let myName = "Carlos";
+
+// Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+let birthCity = "São Paulo"
