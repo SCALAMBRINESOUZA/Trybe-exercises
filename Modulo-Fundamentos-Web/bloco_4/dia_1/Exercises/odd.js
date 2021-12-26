c8ad3bc375629374de4a29ca3dbb6d652133629a
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-26 18:44:21 -0300

    done exercise which return true if number for odd

---
+++ Modulo-Fundamentos-Web/bloco_4/dia_1/Exercises/odd.js
@@ -0,0 +1,9 @@
+const numberOne = 10;
+const numberTwo = 2;
+const numberThree = 33;
+
+if (numberOne % 2 != 0 || numberTwo % 2 != 0 || numberThree % 2 != 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
