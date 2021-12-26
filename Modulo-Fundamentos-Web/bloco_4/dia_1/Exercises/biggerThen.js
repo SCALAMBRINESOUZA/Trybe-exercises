6f2d94f9da441d5c59a67283bdeb370aea0d41f0
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-26 17:35:03 -0300

    done exercise what return larger number with three numbers

--- Modulo-Fundamentos-Web/bloco_4/dia_1/Exercises/biggerThen.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_1/Exercises/biggerThen.js
@@ -1,10 +1,21 @@
 // Faça um programa que retorne o maior de dois números.Defina no começo do programa duas constantes com os valores que serão comparados.
 
-const n1 = 250;
-const n2 = 260;
+ const n1 = 450;
+ const n2 = 360;
+ const n3 = 500;
 
-if(n1 > n2) {
+// if(n1 > n2) {
+//     console.log(n1);
+// } else {
+//     console.log(n2);
+// }
+
+// Faça um programa que retorne o maior de três números.Defina no começo do programa três constantes com os valores que serão comparados.
+
+if(n1 > n2 && n1 > n3) {
     console.log(n1);
-} else {
+} else if(n2 > n1 && n2 > n3) {
     console.log(n2);
+} else {
+    console.log(n3);
 }
