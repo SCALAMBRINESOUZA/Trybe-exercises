8eea2d393cb01a015aad1661cb50ed681d458171
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-26 17:28:31 -0300

    done exercise what return larger number

---
+++ Modulo-Fundamentos-Web/bloco_4/dia_1/Exercises/biggerThen.js
@@ -0,0 +1,10 @@
+// Faça um programa que retorne o maior de dois números.Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const n1 = 250;
+const n2 = 260;
+
+if(n1 > n2) {
+    console.log(n1);
+} else {
+    console.log(n2);
+}
