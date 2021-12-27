186e2fe3a47621d566b0bd70aff1742eee98e596
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-27 09:50:17 -0300

    done example using for/of

--- Modulo-Fundamentos-Web/bloco_4/dia_2/Examples/for.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_2/Examples/for.js
@@ -4,3 +4,10 @@ let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 for(let index = 0; index < groceryList.length; index += 1) {
     console.log(groceryList[index]);
 }
+
+// Utilize o for/of para imprimir os elementos da lista names com o console.log() :
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(firstName of names) {
+    console.log(firstName);
+}
