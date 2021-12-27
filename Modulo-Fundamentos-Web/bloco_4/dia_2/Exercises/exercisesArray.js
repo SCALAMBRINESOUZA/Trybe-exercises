7fc5cf9dc64836180dffb7ae546607168a69e867
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-27 12:11:23 -0300

    done exercises bonus and finished all exercises

--- Modulo-Fundamentos-Web/bloco_4/dia_2/Exercises/exercisesArray.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_2/Exercises/exercisesArray.js
@@ -84,3 +84,41 @@ for(let number = 1; number <= 25; number += 1) {
 for (let number = 1; number < newNumbers.length; number += 1) {
     console.log(newNumbers[number] / 2);
 }
+
+// Ordene o array numbers em ordem crescente e imprima seus valores;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for(let i = 1; i < numbers.length; i += 1) {
+    for(let index = 0; index < numbers.length; index += 1) {
+        if(numbers[i] < numbers[index]) {
+            let position = numbers[i];
+            numbers[i] = numbers[index];
+            numbers[index] = position;
+        }
+    }
+}
+console.log(numbers);
+
+// Ordene o array numbers em ordem decrescente e imprima seus valores;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (let i = 1; i < numbers.length; i += 1) {
+    for (let index = 0; index < numbers.length; index += 1) {
+        if (numbers[i] > numbers[index]) {
+            let position = numbers[i];
+            numbers[i] = numbers[index];
+            numbers[index] = position;
+        }
+    }
+}
+console.log(numbers);
+
+// Agora crie um novo array a partir do array numbers, sem perdê - lo.Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte.Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5(primeiro valor) e 9(valor seguinte).Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9(segundo valor) e 3(valor seguinte), e assim por diante.Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push.O resultado deve ser o array abaixo:
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newArray = [];
+for(let i = 0; i < numbers.length; i += 1) {
+    if(i + 1 < numbers.length) {
+        newArray.push(numbers[i] * numbers[i + 1]);
+    } else {
+        newArray.push(numbers[i] * 2);
+    }
+}
+console.log(newArray);
