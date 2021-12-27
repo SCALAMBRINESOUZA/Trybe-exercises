51c2837927899a3bea975db6d05da72032acf66d
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-27 10:56:07 -0300

    done four exercise

--- Modulo-Fundamentos-Web/bloco_4/dia_2/Exercises/exercisesArray.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_2/Exercises/exercisesArray.js
@@ -23,3 +23,18 @@ for(let i = 0; i < numbers.length; i += 1) {
     average = sum / numbers.length;
 }
 console.log(average);
+
+// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20".Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let average = 0;
+for (let i = 0; i < numbers.length; i += 1) {
+    sum += numbers[i];
+    average = sum / numbers.length;
+};
+if (average > 20) {
+    console.log('Valor maior que 20');
+} else if (average <= 20) {
+    console.log('Valor menor que 20');
+};
+console.log(average);
