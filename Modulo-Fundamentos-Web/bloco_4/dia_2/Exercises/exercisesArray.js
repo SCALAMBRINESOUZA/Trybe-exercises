f28396bc3535aa6793d30af693198e32d120e89b
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-27 10:23:53 -0300

    done second exercise

---
+++ Modulo-Fundamentos-Web/bloco_4/dia_2/Exercises/exercisesArray.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+for(let number of numbers) {
+    console.log(number);
+}
+
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+for(let i = 0; i < numbers.length; i ++) {
+    sum += numbers[i];
+    console.log(sum);
+}
