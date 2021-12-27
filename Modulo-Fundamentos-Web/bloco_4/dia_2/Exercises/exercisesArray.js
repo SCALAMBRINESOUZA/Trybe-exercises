86622d5026424fb55e8d073cf8dc6bce139fcbe4
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-27 11:08:06 -0300

    done five exercise

--- Modulo-Fundamentos-Web/bloco_4/dia_2/Exercises/exercisesArray.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_2/Exercises/exercisesArray.js
@@ -38,3 +38,13 @@ if (average > 20) {
     console.log('Valor menor que 20');
 };
 console.log(average);
+
+// Utilizando for , descubra qual o maior valor contido no array e imprima - o;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maxValue = 0;
+for(let index = 0; index < numbers.length; index += 1) {
+    if(numbers[index] > maxValue) {
+        maxValue = numbers[index]
+    }
+}
+console.log(maxValue);
