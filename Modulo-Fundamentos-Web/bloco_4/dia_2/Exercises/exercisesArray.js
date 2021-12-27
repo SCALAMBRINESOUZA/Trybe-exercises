fdf854c82884272cf257580cb5a9f107b0ac881a
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-27 11:21:04 -0300

    done seven exercise

--- Modulo-Fundamentos-Web/bloco_4/dia_2/Exercises/exercisesArray.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_2/Exercises/exercisesArray.js
@@ -62,3 +62,14 @@ for (let index = 0; index < numbers.length; index += 1) {
     } else {
         console.log(numbersOdd);
     }
+
+// Utilizando for , descubra qual o menor valor contido no array e imprima - o;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let minValue = 100;
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] < minValue) {
+        minValue = numbers[index]
+    }
+}
+console.log(minValue);
+
