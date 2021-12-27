8e6f8b1d0fd5d1d410a10ce5323d431e7e430c26
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-27 08:57:42 -0300

    done second example

--- Modulo-Fundamentos-Web/bloco_4/dia_2/Examples/arrays.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_2/Examples/arrays.js
@@ -3,3 +3,9 @@ let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 
 console.log(menuServices);
+
+// Procure o índice do valor "Portfólio" do array menu :
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu[2];
+
+console.log(indexOfPortfolio);
