07ac608bb067182b8dd08566d7195277585a6892
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-27 10:40:16 -0300

    done third exercise

--- Modulo-Fundamentos-Web/bloco_4/dia_2/Exercises/exercisesArray.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_2/Exercises/exercisesArray.js
@@ -9,7 +9,17 @@ for(let number of numbers) {
 // Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
-for(let i = 0; i < numbers.length; i ++) {
+for(let i = 0; i < numbers.length; i += 1) {
     sum += numbers[i];
-    console.log(sum);
 }
+console.log(sum);
+
+// Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let average = 0;
+for(let i = 0; i < numbers.length; i += 1) {
+    sum += numbers[i];
+    average = sum / numbers.length;
+}
+console.log(average);
