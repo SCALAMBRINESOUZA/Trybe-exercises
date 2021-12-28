a88f44be0962f87380aac9c12c72adcb569b163f
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-28 17:42:47 -0300

    rename first exercise and add second exercise

---
+++ Modulo-Fundamentos-Web/bloco_4/dia_3/secondExercise.js
@@ -0,0 +1,9 @@
+// Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.Por exemplo:
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex <= n; lineIndex += 1) {
+    console.log(inputLine);
+    inputLine = inputLine + symbol;
+}
