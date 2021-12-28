eaf47c7248b38a5a96c9ad489fb2af8a7f5df9ef
ScalambrineSouza <henrique.joaovictor@hotmail.com>
2021-12-28 19:26:30 -0300

    Delete NumeroPrimo.js

--- Modulo-Fundamentos-Web/bloco_4/dia_2/NumeroPrimo.js
+++
@@ -1,8 +0,0 @@
-// const primo = number => number % 2 !== 0 ? console.log(`Sim ${number} é primo`) 
-// : console.log(`${number} não é primo`);
-// primo(38); 
-
-        // mesmo exercicio sem o console log tornando mais curta a expressao //
- 
- const primo = number => number % 2 === 0 ? `${number} não é primo` : `${number} sim é primo`;       
- console.log(primo(40)); 
