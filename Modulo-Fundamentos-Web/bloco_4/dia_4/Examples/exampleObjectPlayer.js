85d2d3df649c52c6b285f68663575beac32c7e42
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-29 17:36:57 -0300

    access the key bestInTheWorld to use property length

--- Modulo-Fundamentos-Web/bloco_4/dia_4/Examples/exampleObjectPlayer.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_4/Examples/exampleObjectPlayer.js
@@ -16,3 +16,7 @@ console.log(`A jogadora ${player.name} ${player['lastName']} tem ${player['age']
 // Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo.
 
 player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018]
+
+// Acesse a chave bestInTheWorld e faça um console.log no seguinte formato: "A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes".
+
+console.log(`A jogadora ${player.name} ${player.lastName} foi eleita a melhor do mundo por ${player.bestInTheWorld.length} vezes`)
