a6a08821de6d88174d028108debaceaf44b186b9
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-29 17:28:52 -0300

    add new key in the object

--- Modulo-Fundamentos-Web/bloco_4/dia_4/Examples/exampleObjectPlayer.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_4/Examples/exampleObjectPlayer.js
@@ -12,3 +12,7 @@ let player = {
 // Acesse as chaves name, lastName e age e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
 
 console.log(`A jogadora ${player.name} ${player['lastName']} tem ${player['age']} anos de idade.`)
+
+// Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo.
+
+player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018]
