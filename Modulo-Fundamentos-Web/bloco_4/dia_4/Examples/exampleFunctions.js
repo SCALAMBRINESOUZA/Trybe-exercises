c1b2bf4d21c6fb85258f160048ae389676f3bd38
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-12-29 22:30:23 -0300

    create function with mathematics operation

---
+++ Modulo-Fundamentos-Web/bloco_4/dia_4/Examples/exampleFunctions.js
@@ -0,0 +1,37 @@
+// Faça cinco programas, um para cada operação aritmética básica.Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados.Faça programas para: 
+
+// Adição(a + b)
+// Subtração(a - b)
+// Multiplicação(a * b)
+// Divisão(a / b)
+// Módulo(a % b)
+
+// observação programas refatoradas para usar funções
+
+function sum(a, b) {
+  return a + b
+};
+
+function sub(a, b) {
+  return a - b
+};
+
+function multiply(a, b) {
+  return a * b
+};
+
+function div(a, b) {
+  return a / b
+};
+
+function mod(a, b) {
+  return a % b
+};
+
+
+console.log(sum(10,20));
+console.log(sub(100, 20));
+console.log(multiply(10, 20));
+console.log(div(30, 20));
+console.log(mod(20, 10));
+
