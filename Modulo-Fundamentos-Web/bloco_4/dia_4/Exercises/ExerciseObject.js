ff6bea8e29d02fd5ab05c5e4f6bf1d2a454956ee
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-01-01 05:16:20 -0300

    done second exercise

--- Modulo-Fundamentos-Web/bloco_4/dia_4/Exercises/ExerciseObject.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_4/Exercises/ExerciseObject.js
@@ -6,4 +6,10 @@ let info = {
 
 // imprima no console uma mensagem de boas - vindas para a personagem acima, incluindo seu nome.Valor esperado no console:
 
-console.log(`Bem-vinda, ${info.personagem}`)
+// console.log(`Bem-vinda, ${info.personagem}`);
+
+// Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.Valor esperado no console:
+
+info['recorrente'] = 'Sim';
+
+console.log(info);
