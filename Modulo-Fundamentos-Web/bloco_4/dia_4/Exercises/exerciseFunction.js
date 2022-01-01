fc0d2cc0598348bb0d8c19e14f313984ecd4db4d
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-01-01 07:27:22 -0300

    done second exercises about function

--- Modulo-Fundamentos-Web/bloco_4/dia_4/Exercises/exerciseFunction.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_4/Exercises/exerciseFunction.js
@@ -1,15 +1,29 @@
-function checkPalindrom(phrase) {
-  let char = phrase.split(''),
-  firstLetter = char.shift(),
-  lastLetter = char.pop();
-  if (firstLetter !== lastLetter ) {
-    return false;
-  }
-  if (char.length < 2) {
-    return true;
+// function checkPalindrom(phrase) {
+//   let char = phrase.split(''),
+//   firstLetter = char.shift(),
+//   lastLetter = char.pop();
+//   if (firstLetter !== lastLetter ) {
+//     return false;
+//   }
+//   if (char.length < 2) {
+//     return true;
+//   }
+
+//   return checkPalindrom(char.join(''));
+// }
+
+// console.log(checkPalindrom('ana'));
+
+// Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+function biggestValue(testArray) {
+  let largerIndex = 0;
+  for(let index in testArray) {
+      if (testArray[largerIndex] < testArray[index]) {
+        largerIndex = index
+      }
+    }
+    return largerIndex;
   }
 
-  return checkPalindrom(char.join(''));
-}
-
-console.log(checkPalindrom('ana'))
+console.log(biggestValue([2, 13, 6, 7, 10, 11]))
