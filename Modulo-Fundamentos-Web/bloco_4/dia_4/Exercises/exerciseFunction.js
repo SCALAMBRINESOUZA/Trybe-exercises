fa5ae12fe6dcbb6bcd377228d238aaf6ae49f062
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-01-01 21:40:29 -0300

    done five exercises about function

--- Modulo-Fundamentos-Web/bloco_4/dia_4/Exercises/exerciseFunction.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_4/Exercises/exerciseFunction.js
@@ -43,14 +43,58 @@
 
 // Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres
 
-function biggestName(testArray) {
-  let maxCount = testArray[0];
-  for (let char of testArray) {
-      if(maxCount.length < char.length) {
-        maxCount = char
-      }        
-    }
-      console.log(maxCount)
-    }
-  
-biggestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'])
+// function biggestName(testArray) {
+//   let maxCount = testArray[0];
+//   for (let char of testArray) {
+//       if(maxCount.length < char.length) {
+//         maxCount = char
+//       }
+//     }
+//       console.log(maxCount)
+//     }
+
+// biggestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'])
+
+// Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+// function numberRepeat(testArray) {
+//     let countRepeat = testArray[0];
+//     for (let number of testArray) {
+//       if (countRepeat == number) {
+//         countRepeat = number
+//       }
+//     }
+//     console.log(countRepeat);
+//   }
+
+
+// numberRepeat([2, 3, 2, 5, 8, 2, 3])
+
+// Exemplo abaixo copiado do course
+
+// function maisRepetido(numeros) {
+//   let num = {};
+
+//   for (let index = 0; index < numeros.length; index += 1) {
+//     let valor = numeros[index];
+//     if (num[valor] === undefined) {
+//       num[valor] = 1;
+//     } else {
+//       num[valor] = num[valor] + 1;
+//     }
+//   }
+
+//   let contRepetido = 0;
+//   let contNumero = 0;
+
+//   for (let prop in num) {
+//     if (contRepetido < num[prop]) {
+//       contRepetido = num[prop];
+//       contNumero = prop;
+//     }
+//   }
+
+//   return contNumero;
+// }
+
+// console.log(maisRepetido([2, 3, 3, 5, 8, 2, 3])); //2
