bdd2a87f061fcbe54da4cff3b6ada5486a60abac
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-02-01 21:05:05 -0300

    fix lint of python

--- Modulo-Ciencias-Da-Computação/bloco_32/day_One/Examples/conditionalStruct.py
+++ Modulo-Ciencias-Da-Computação/bloco_32/day_One/Examples/conditionalStruct.py
@@ -24,7 +24,12 @@
 # print("Factorial of", number, "is", factorial(5))
 
 
-# Um sistema de avaliações registra valores de 0 a 10 para cada avaliação. Após algumas mudanças estes valores precisam ser ajustados para avaliações de 0 a 100. Dado uma sequência de avaliações ratings = [6, 8, 5, 9, 10] . Escreva um código capaz de gerar as avaliações após a mudança. Neste caso o resultado deveria ser [60, 80, 50, 90, 100]
+# Um sistema de avaliações registra valores de 0 a 10 para cada avaliação.
+# Após algumas mudanças estes valores precisam
+# ser ajustados para avaliações de 0 a 100.
+# Dado uma sequência de avaliações ratings = [6, 8, 5, 9, 10].
+# Escreva um código capaz de gerar as avaliações após a mudança.
+# Neste caso o resultado deveria ser [60, 80, 50, 90, 100]
 
 rattings = [6, 8, 5, 9, 10]
 # new_rattings = []
@@ -33,7 +38,8 @@ rattings = [6, 8, 5, 9, 10]
 #     new_rattings.append(rating * 10)
 # print(new_rattings)
 
-# Exercício 15: Percorra a lista do exercício 14 e imprima "Múltiplo de 3" se o elemento for divisível por 3.
+# Exercício 15: Percorra a lista do exercício 14 e imprima
+# "Múltiplo de 3" se o elemento for divisível por 3.
 
 for rating in rattings:
     if (rating % 3) == 0:
