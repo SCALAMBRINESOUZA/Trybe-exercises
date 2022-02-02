d532fc1d7c3e18c685d2f0e3841f92587d033b75
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-02-02 00:04:39 -0300

    done exercise of average

---
+++ Modulo-Ciencias-Da-Computação/bloco_32/day_One/Exercises/average.py
@@ -0,0 +1,21 @@
+# def average_arithmetic(list):
+#     print(sum(list) / len(list))
+
+# average_arithmetic([8, 10, 15])    
+
+
+import statistics, numpy
+
+
+def average_arithmetic(list):
+    print(statistics.mean(list))
+    
+average_arithmetic([8, 10, 15])    
+
+
+def average_arithmetic(list):
+    print(numpy.mean(list))
+    
+average_arithmetic([8, 10, 15])    
+
+
