9ad4e3b403ffbd9c73de0bca3dcfaead5ed30099
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-02-02 19:11:39 -0300

    done exercise greate name

---
+++ Modulo-Ciencias-Da-Computação/bloco_32/day_One/Exercises/greater_name.py
@@ -0,0 +1,14 @@
+list_name = ["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"]
+
+
+def find_biggest_name(list):
+    print(max(list, key=len))
+    # biggest_name = list[0]
+    # print(biggest_name)
+    # for li in list:
+    #     if len(li) > len(biggest_name):
+    #         biggest_name = li
+    # return biggest_name
+
+
+find_biggest_name(list_name)
