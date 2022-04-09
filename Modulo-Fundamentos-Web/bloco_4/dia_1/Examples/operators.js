27acd105d5a199417784f1742982e23777f8a378
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-04-08 21:50:27 -0300

    update and review

--- Modulo-Fundamentos-Web/bloco_4/dia_1/Examples/operators.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_1/Examples/operators.js
@@ -1,59 +1,59 @@
-// // Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100;
-// const nota = 80;
+// // // Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100;
+// // const nota = 59;
 
-// /* Implemente uma lógica que verifique se a pessoa candidata foi aprovada, reprovada ou se ela está na lista de espera. Para isso, considere as seguintes informações:
-// Se a nota for maior ou igual a 80, imprima "Parabéns, você foi aprovada(o)!"
-// Se a nota for menor que 80 e maior ou igual a 60, imprima "Você está na nossa lista de espera"
-// Se a nota for menor que 60, imprima "Você foi reprovada(o)"
+// // /* Implemente uma lógica que verifique se a pessoa candidata foi aprovada, reprovada ou se ela está na lista de espera. Para isso, considere as seguintes informações:
+// // Se a nota for maior ou igual a 80, imprima "Parabéns, você foi aprovada(o)!"
+// // Se a nota for menor que 80 e maior ou igual a 60, imprima "Você está na nossa lista de espera"
+// // Se a nota for menor que 60, imprima "Você foi reprovada(o)"
+// // */
+// // if(nota >= 80) {
+// //   console.log('Parabéns, você foi aprovada(o)!');
+// // } else if(nota < 80 && nota >= 60) {
+// //   console.log('Você está na nossa lista de espera');
+// // } else {
+// //   console.log('Você foi reprovada(o)');
+// // }
+
+// // Crie uma constante chamada "currentHour" que recebe um número entre 4 e 24 de sua escolha, para representar as horas do dia.
+// // const currentHour = 21;
+
+// // // Crie uma variável chamada "message" que, inicialmente, é uma string vazia.
+// // let message = null; 
+
+// // /*
+// // Implemente condicionais para que:
+// // Se o horário for maior ou igual a 22, insira "Não deveríamos comer nada, é hora de dormir" na variável "message".
+// // Se o horário for maior ou igual a 18 e menor que 22, insira "Rango da noite, vamos jantar :D" na variável "message".
+// // Se o horário for maior ou igual a 14 e menor que 18, insira "Vamos fazer um bolo pro café da tarde?" na variável "message".
+// // Se o horário estiver entre 11 e 14, insira "Hora do almoço!!!" na variável "message".
+// // Se o horário estiver entre 4 e 11, insira "Hmmm, cheiro de café recém passado" na variável "message".
+// // */
+
+// // if(currentHour >= 22) {
+// //   message = 'Não deveríamos comer nada, é hora de dormir';
+// // } else if(currentHour >= 18 && currentHour < 22) {
+// //   message = 'Rango da noite, vamos jantar';
+// // } else if(currentHour >= 14 && currentHour < 18) {
+// //   message = 'Vamos fazer um bolo pro café da tarde?';
+// // } else if(currentHour >= 11 && currentHour <= 14) {
+// //   message = 'Hora do almoço!!!';
+// // } else if(currentHour >= 4 && currentHour <= 11) {
+// //   message = 'Hmmm, cheiro de café recém passado'
+// // }
+
+// // console.log(message);
+
+// // Crie uma variável chamada "weekDay" que recebe a string "quarta-feira".
+// let weekDay = 'sexta-feira';
+
+// /*
+// Implemente condicionais para que:
+// Se nossa variável "weekDay" for "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira" ou "sexta-feira", imprima "Oba, mais um dia de aprendizado na Trybe >:D".
+// Se for algum dia de fim de semana, imprima "FINALMENTE, descanso merecido UwU".
 // */
-// if(nota >= 80) {
-//   console.log('Parabéns, você foi aprovada(o)!');
-// } else if(nota < 80 && nota >= 60) {
-//   console.log('Você está na nossa lista de espera');
-// } else {
-//   console.log('Você foi reprovada(o)');
+
+// if(weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+//   console.log('Oba, mais um dia de aprendizado na Trybe >:D');
+// } else if(weekDay === 'sabado' || weekDay === 'domingo') {
+//   console.log('FINALMENTE, descanso merecido UwU');
 // }
-
-// Crie uma constante chamada "currentHour" que recebe um número entre 4 e 24 de sua escolha, para representar as horas do dia.
-// const currentHour = 4;
-
-// Crie uma variável chamada "message" que, inicialmente, é uma string vazia.
-// let message = ''; 
-
-/*
-Implemente condicionais para que:
-Se o horário for maior ou igual a 22, insira "Não deveríamos comer nada, é hora de dormir" na variável "message".
-Se o horário for maior ou igual a 18 e menor que 22, insira "Rango da noite, vamos jantar :D" na variável "message".
-Se o horário for maior ou igual a 14 e menor que 18, insira "Vamos fazer um bolo pro café da tarde?" na variável "message".
-Se o horário estiver entre 11 e 14, insira "Hora do almoço!!!" na variável "message".
-Se o horário estiver entre 4 e 11, insira "Hmmm, cheiro de café recém passado" na variável "message".
-*/
-
-// if(currentHour >= 22) {
-//   message = 'Não deveríamos comer nada, é hora de dormir';
-// } else if(currentHour >= 18 && currentHour < 22) {
-//   message = 'Rango da noite, vamos jantar';
-// } else if(currentHour >= 14 && currentHour < 18) {
-//   message = 'Vamos fazer um bolo pro café da tarde?';
-// } else if(currentHour >= 11 && currentHour <= 14) {
-//   message = 'Hora do almoço!!!';
-// } else if(currentHour >= 4 && currentHour <= 11) {
-//   message = 'Hmmm, cheiro de café recém passado'
-// }
-
-// console.log(message);
-
-// Crie uma variável chamada "weekDay" que recebe a string "quarta-feira".
-let weekDay = 'domingo';
-
-/*
-Implemente condicionais para que:
-Se nossa variável "weekDay" for "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira" ou "sexta-feira", imprima "Oba, mais um dia de aprendizado na Trybe >:D".
-Se for algum dia de fim de semana, imprima "FINALMENTE, descanso merecido UwU".
-*/
-
-if(weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
-  console.log('Oba, mais um dia de aprendizado na Trybe >:D');
-} else if(weekDay === 'sabado' || weekDay === 'domingo') {
-  console.log('FINALMENTE, descanso merecido UwU');
-}
