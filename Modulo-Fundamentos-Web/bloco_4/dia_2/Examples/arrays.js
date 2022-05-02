97de5cca1b6d9a3c88d6bf239603eeceeb388b4b
ScalambrineSouza <henrique.joaovictor@hotmail.com>
2022-05-02 17:01:34 -0300

    correction in the solution of problem

--- Modulo-Fundamentos-Web/bloco_4/dia_2/Examples/arrays.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_2/Examples/arrays.js
@@ -6,7 +6,7 @@ console.log(menuServices);
 
 // Procure o índice do valor "Portfólio" do array menu :
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let indexOfPortfolio = menu[2];
+let indexOfPortfolio = menu.indexOf('Portfólio');
 
 console.log(indexOfPortfolio);
 
