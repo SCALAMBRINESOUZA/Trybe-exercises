1ca9645f39339583c637eb35788617ad9603ca04
ScalambrineSouza <henrique.joaovictor@hotmail.com>
2022-05-04 20:21:45 -0300

    Add exercise

--- Modulo-Fundamentos-Web/bloco_4/dia_3/exercicios_fixacao/thirdExercise.js
+++ Modulo-Fundamentos-Web/bloco_4/dia_3/exercicios_fixacao/thirdExercise.js
@@ -1,1 +1,20 @@
 // Agora inverta o lado do triângulo.Por exemplo:
+let n = 5;
+let esp = "  "
+let symbol = '*';
+let inputLine = '';
+let inputPosition = n;
+
+for (let lineIndex = 0; lineIndex <= n; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex < inputPosition) {
+      inputLine = inputLine + ' ';
+    } else {
+      inputLine = inputLine + symbol;
+    }
+  }
+
+  console.log(esp + inputLine);
+  inputLine = '';
+  inputPosition -= 1;
+}
