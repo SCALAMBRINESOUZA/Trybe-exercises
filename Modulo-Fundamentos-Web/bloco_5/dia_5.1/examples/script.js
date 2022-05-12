7ba46f99779ae7316e4323b4ddd1f983dbe4243c
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-05-12 17:26:13 -0300

    again alter place on the file index2.html and script2.js, style.css

--- Modulo-Fundamentos-Web/bloco_5/dia_5.1/examples/script.js
+++ Modulo-Fundamentos-Web/bloco_5/dia_5.1/examples/script.js
@@ -1,19 +1,33 @@
-// var paragraph = document.getElementById("paragraph");
-//     paragraph.style.color = "red";
+// // var paragraph = document.getElementById("paragraph");
+// //     paragraph.style.color = "red";
 
 // let title = document.querySelector('#page-title');
-//     title.innerText = 'Bullet';
+//     title.innerText = 'Até as ultimas consequencias';
     
 // let secondParagraph = document.querySelector('#second-paragraph');
 //     secondParagraph.innerHTML = "HELLO WORLD DOM"
+//     secondParagraph.style.color = 'red'
+//     secondParagraph.style.background = 'blue'
+//     secondParagraph.style.width = '160px'
+//     secondParagraph.style.padding = '5px'
+  
 
 // let subTitle = document.querySelector('#subtitle');
 //     subTitle.innerText = 'Let´s go learn javascript'    
 //     subTitle = document.getElementsByTagName('h4')[0];
 //     subTitle.innerText = 'Let´s go learn html css and javascript' 
+//     subTitle.style.background = 'red'
+//     subTitle.style.width = '260px'
+//     subTitle.style.color = 'blue'
+//     subTitle.style.padding = '5px'
 
-// let paragraphs = document.getElementsByClassName('par');
-// for (let index = 0; index < paragraphs.length; index += 1) {
-//     paragraphs[index].style.color = 'blue' 
-//     paragraphs[0].style.background = 'gray' 
-// }    
+
+
+// let paragraphs = document.querySelectorAll('.paragraphs')[0];
+//     paragraphs.style.border = '5px solid green'
+//     paragraphs.style.width = '210px'
+//     paragraphs.style.color = 'brown'
+// // for (let index = 0; index < paragraphs.length; index += 1) {
+// //     paragraphs[index].style.color = 'blue' 
+// //     paragraphs[0].style.background = 'gray' 
+// // }    
